e129be6af8e4658a86aa21bcf2890ed9ed4fcea9
AkashSDas <aakashdas368@gmail.com>
2021-09-08 09:34:10 +0530

    create routes for food category

---
+++ backend/src/routes/food-category.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
+import createCategory from "../controllers/food-category/create";
+import deleteCategory from "../controllers/food-category/delete";
+import getAllCategories from "../controllers/food-category/get-all-categories";
+import getCategory from "../controllers/food-category/get-category";
+import { getCategoryById } from "../controllers/food-category/middlewares";
+import updateCategory from "../controllers/food-category/update";
+import { getMainUserById } from "../controllers/main-user/middlewares";
+
+export const router = Router();
+
+/// Params
+router.param("userId", getMainUserById);
+router.param("foodCategoryId", getCategoryById);
+
+/// Routes
+router.post("/:userId", isSignedIn, isAuthenticated, createCategory);
+
+router.put(
+  "/:foodCategoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  updateCategory
+);
+
+router.delete(
+  "/:foodCategoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  deleteCategory
+);
+
+router.get(
+  "/:foodCategoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  getCategory
+);
+
+router.get("/:userId", isSignedIn, isAuthenticated, getAllCategories);
