a0841fa663d01505def62de73e1718529d9491e2
AkashSDas <aakashdas368@gmail.com>
2021-09-08 20:01:59 +0530

    parse form data in create blog post and upload cover img to firebase storage

--- backend/src/controllers/blog-post/create.ts
+++ backend/src/controllers/blog-post/create.ts
@@ -1,28 +1,150 @@
 import { Request, Response } from "express";
-import BlogPost, { BlogPostDocument } from "../../models/blog-post";
+import BlogPost from "../../models/blog-post";
 import { runAsync } from "../../utils";
-import { expressValidatorErrorResponse, responseMsg } from "../json-response";
+import formidable, { File } from "formidable";
+import { v4 } from "uuid";
+import { bucket } from "../../firebase";
+import { responseMsg } from "../json-response";
 
-/// TODO: add formdata parser and upload image to firebase
 async function createBlogPost(req: Request, res: Response) {
-  const [errors, jsonRes] = expressValidatorErrorResponse(req, res);
-  if (errors) return jsonRes;
+  let form = new formidable.IncomingForm({ keepExtensions: true });
 
-  const category = new BlogPost(req.body);
-  const [data, err] = await runAsync(category.save());
-  if (err)
-    return responseMsg(res, {
-      status: 400,
-      message: "Not able to create post",
-    });
+  form.parse(
+    req,
+    async (err: any, fields: formidable.Fields, files: formidable.Files) => {
+      if (err) {
+        // If user is coming till here after passing all vaidators
+        // then chances are high that the problem is in file itself
+        return responseMsg(res, {
+          status: 400,
+          message: "There is some problem with the image file",
+        });
+      }
 
-  const savedPost: BlogPostDocument = data;
-  return responseMsg(res, {
-    status: 200,
-    error: false,
-    message: "Post created successfully",
-    data: { post: savedPost },
-  });
+      const { title, description, content, readTime: readTimeStr } = fields;
+      const { categories: categoriesStr, author } = fields;
+      const { coverImg } = files;
+
+      if (
+        !title ||
+        !description ||
+        !content ||
+        !readTimeStr ||
+        !categoriesStr ||
+        !author ||
+        !coverImg
+      ) {
+        return responseMsg(res, {
+          status: 400,
+          message: "Please include all the fields",
+        });
+      }
+
+      /// Currently unable to send array of object ids from postman
+      /// using all the ways found (along with by Postman doc) only
+      /// the last id was received in backend in the form of text (string)
+      /// Way used was
+      /// Key              |  Value
+      /// categories       | 6138407f6b5436edd3415e6a
+      /// categories       | 61386910b033172f938b5427
+      ///
+      /// Currently you've to send category ids in the form given below
+      /// '6138407f6b5436edd3415e6a,61386910b033172f938b5427'
+      /// i.e.
+      /// Key              |  Value
+      /// categories       | 6138407f6b5436edd3415e6a,61386910b033172f938b5427
+      /// which is parsed here
+
+      let categories;
+      try {
+        categories = (categoriesStr as string).trim().split(",");
+      } catch (er) {
+        return responseMsg(res, {
+          status: 400,
+          message: "Wrong format used for sending categories",
+        });
+      }
+
+      let readTime;
+      try {
+        readTime = parseFloat(readTimeStr as string);
+      } catch (er) {
+        return responseMsg(res, {
+          status: 400,
+          message: "Read time should be a number",
+        });
+      }
+
+      const data = {
+        title,
+        description,
+        content,
+        categories,
+        readTime,
+        author,
+      };
+
+      let blogPost = new BlogPost(data);
+
+      /// Save cover img in firebase storage
+      const userId = req.profile._id;
+      const filename = (coverImg as File).name;
+
+      const uuid = v4();
+      const destination = `blog-post-cover-imgs/${userId}/${blogPost._id}/${filename}`;
+      const [_, error] = await runAsync(
+        bucket.upload((coverImg as File).path, {
+          destination,
+          metadata: {
+            contentType: "image/png",
+            metadata: {
+              firebaseStorageDownloadTokens: uuid,
+            },
+          },
+        })
+      );
+
+      if (error)
+        return responseMsg(res, {
+          status: 400,
+          message: "Could not save cover image",
+        });
+
+      const photoURL =
+        "https://firebasestorage.googleapis.com/v0/b/" +
+        bucket.name +
+        "/o/" +
+        encodeURIComponent(destination) +
+        "?alt=media&token=" +
+        uuid;
+
+      blogPost.coverImgURL = photoURL;
+
+      const [savedBlogPost, e] = await runAsync(blogPost.save());
+      if (e)
+        return responseMsg(res, {
+          status: 400,
+          message: "Could not save post",
+        });
+
+      return responseMsg(res, {
+        status: 200,
+        error: false,
+        message: "Successfully saved post",
+        data: {
+          post: {
+            _id: savedBlogPost._id,
+            title: savedBlogPost.title,
+            description: savedBlogPost.description,
+            coverImgURL: savedBlogPost.coverImgURL,
+            readTime: savedBlogPost.readTime,
+            categories: savedBlogPost.categories,
+            author: savedBlogPost.author,
+          },
+        },
+      });
+    }
+  );
 }
 
 export default createBlogPost;
