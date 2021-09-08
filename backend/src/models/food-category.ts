95614ac924b520f3891cc02a85fa6b4cb809ee0b
AkashSDas <aakashdas368@gmail.com>
2021-09-08 09:12:17 +0530

    create mongoose model for food category

---
+++ backend/src/models/food-category.ts
@@ -0,0 +1,36 @@
+import { Document, Schema, model } from "mongoose";
+
+export type FoodCategoryDocument = Document & {
+  emoji: string;
+  name: string;
+  description: string;
+};
+
+const FoodCategorySchema = new Schema<FoodCategoryDocument>({
+  emoji: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 1,
+    trim: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 16,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 1024,
+    trim: true,
+  },
+});
+
+const FoodCategory = model<FoodCategoryDocument>(
+  "FoodCategory",
+  FoodCategorySchema
+);
+export default FoodCategory;
