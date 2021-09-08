35402179d74d0cb1d85a408770f366422146db82
AkashSDas <aakashdas368@gmail.com>
2021-09-08 11:46:25 +0530

    create blog post model

---
+++ backend/src/models/blog-post.ts
@@ -0,0 +1,34 @@
+import { Document, model, Schema } from "mongoose";
+import { FoodCategorySchema } from "./food-category";
+import { MainUserDocument } from "./main-user";
+import MongoPaging from "mongo-cursor-pagination";
+
+export type BlogPostDocument = Document & {
+  title: string;
+  description: string;
+  content: string;
+  coverImgURL: string;
+  readTime: number;
+  createdAt: Date;
+  updatedAt: Date;
+  categories: any;
+  author: MainUserDocument;
+};
+
+const BlogPostSchema = new Schema<BlogPostDocument>(
+  {
+    title: { type: String, required: true, maxlength: 256, trim: true },
+    description: { type: String, required: true, maxlength: 2048, trim: true },
+    content: { type: String, required: true, trim: true },
+    coverImgURL: { type: String, required: true },
+    readTime: { type: Number, required: true },
+    categories: { type: [FoodCategorySchema], required: true },
+    author: { type: Schema.Types.ObjectId, ref: "MainUser", required: true },
+  },
+  { timestamps: true }
+);
+
+BlogPostSchema.plugin(MongoPaging.mongoosePlugin, { name: "paginateBlogPost" });
+
+const BlogPost = model<BlogPostDocument>("BlogPost", BlogPostSchema);
+export default BlogPost;
