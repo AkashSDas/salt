b51f025410734dfdba66d301c6ae5827d4579cf8
AkashSDas <aakashdas368@gmail.com>
2021-09-09 09:32:50 +0530

    create recipe mongoose model

---
+++ backend/src/models/recipe.ts
@@ -0,0 +1,40 @@
+import { Document, model, Schema } from "mongoose";
+import { MainUserDocument } from "./main-user";
+import MongoPaging from "mongo-cursor-pagination";
+
+export type RecipeDocument = Document & {
+  title: string;
+  description: string;
+  content: string;
+  coverImgURL?: string;
+  readTime: number;
+  createdAt: Date;
+  updatedAt: Date;
+  ingredients: any;
+  categories: any;
+  author: MainUserDocument;
+};
+
+const RecipeSchema = new Schema<RecipeDocument>(
+  {
+    title: { type: String, required: true, trim: true, maxlength: 256 },
+    description: { type: String, required: true, maxlength: 2048, trim: true },
+    content: { type: String, required: true, trim: true },
+    coverImgURL: { type: String },
+    readTime: { type: Number, required: true },
+    categories: {
+      type: [{ type: Schema.Types.ObjectId, ref: "FoodCategory" }],
+      required: true,
+    },
+    ingredients: {
+      type: [{ type: Schema.Types.ObjectId, ref: "Ingredient" }],
+    },
+    author: { type: Schema.Types.ObjectId, ref: "MainUser", required: true },
+  },
+  { timestamps: true }
+);
+
+RecipeSchema.plugin(MongoPaging.mongoosePlugin, { name: "paginateRecipe" });
+
+const Recipe = model<RecipeDocument>("Recipe", RecipeSchema);
+export default Recipe;
