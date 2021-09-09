4a06bf4e826ffa2daf5b9405f4c3b771d381626d
AkashSDas <aakashdas368@gmail.com>
2021-09-09 18:58:06 +0530

    style the container and create header widget in signup screen

--- flutter-app/lib/screens/auth/signup.dart
+++ flutter-app/lib/screens/auth/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salt/designs/designs.dart';
 
 class SignupScreen extends StatelessWidget {
   @override
@@ -7,10 +8,62 @@ class SignupScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
         body: Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(32),
+            boxShadow: DesignSystem.subtleBoxShadow,
+          ),
           child: Column(
-            children: [Text('Signup')],
+            children: [
+              Header(),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 32),
+      child: Column(
+        children: [
+          Text(
+            'salt',
+            style: Theme.of(context).textTheme.headline1?.copyWith(
+              fontSize: 60,
+              fontWeight: FontWeight.w900,
+              shadows: [
+                Shadow(
+                  offset: Offset(0, 16),
+                  blurRadius: 32,
+                  color: Color(0xffFFC21E).withOpacity(0.3),
+                ),
+                Shadow(
+                  offset: Offset(0, -8),
+                  blurRadius: 32,
+                  color: Color(0xffFFC21E).withOpacity(0.3),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 32),
+            child: Text(
+              'All you need for your greedy stomach',
+              style: Theme.of(context).textTheme.bodyText2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
