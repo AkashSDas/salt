7593041385fa7603ae01c04a96e9ab2ded0400f6
AkashSDas <aakashdas368@gmail.com>
2021-09-09 08:38:09 +0530

    create routes and param for new controllers and blogPostId

--- backend/src/routes/blog-post.ts
+++ backend/src/routes/blog-post.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
+import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import createBlogPost from "../controllers/blog-post/create";
+import getAllBlogPosts from "../controllers/blog-post/get-all-blog-posts";
+import getBlogPost from "../controllers/blog-post/get-blog-post";
+import { getBlogPostById } from "../controllers/blog-post/middlewares";
+import updateBlogPost from "../controllers/blog-post/update";
 import { getMainUserById } from "../controllers/main-user/middlewares";
 
 export const router = Router();
 
 /// Params
 router.param("userId", getMainUserById);
+router.param("blogPostId", getBlogPostById);
 
 /// Routes
 router.post("/:userId", createBlogPost);
+router.put("/:blogPostId/:userId", isSignedIn, isAuthenticated, updateBlogPost);
+router.get("/", getAllBlogPosts);
+router.get("/:blogPostId", getBlogPost);
