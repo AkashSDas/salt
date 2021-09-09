09fec7e1e7cd4ac5c1928c73cef2689b1a006934
AkashSDas <aakashdas368@gmail.com>
2021-09-09 18:26:42 +0530

    create route for signup screen and routing to it in home screen

--- flutter-app/lib/screens/home.dart
+++ flutter-app/lib/screens/home.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: Container(
+          child: Column(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/auth/signup');
+                  },
+                  child: Text('Sign up'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
