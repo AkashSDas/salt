d838dc990a85e1790ffdbe9b160966a90162d2f8
AkashSDas <aakashdas368@gmail.com>
2021-09-09 18:17:33 +0530

    add theme and routes with home route wrapped in a splash screen widget in main.dart

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:salt/designs/designs.dart';
+import 'package:salt/screens/home.dart';
 
 /// Run app
 void main() => runApp(MyApp());
@@ -7,11 +10,27 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Salt',
-      debugShowCheckedModeBanner: false,
-      home: Container(),
-    );
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Salt',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: DesignSystem.grey0,
+          textTheme: DesignSystem.textTheme,
+        ),
+        routes: _getRoutes(context),
+      );
+
+  Map<String, Widget Function(BuildContext)> _getRoutes(BuildContext context) {
+    return {
+      '/': (context) => SplashScreen.navigate(
+            height: 90,
+            width: 90,
+            name: 'assets/flare/logo.flr',
+            next: (context) => HomeScreen(),
+            until: () => Future.delayed(Duration(seconds: 3)),
+            startAnimation: 'idle',
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+    };
   }
 }
