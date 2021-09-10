b24f6aba9f2eee06359c80c4dd9d616172448369
AkashSDas <aakashdas368@gmail.com>
2021-09-10 13:57:16 +0530

    create submit method for signup - without singup feature

--- flutter-app/lib/widgets/auth-screen/signup.dart
+++ flutter-app/lib/widgets/auth-screen/signup.dart
@@ -28,11 +28,38 @@ class _SignupState extends State<Signup> {
   ]);
 
   final _passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'Password is required'),
     MinLengthValidator(
       6,
       errorText: 'Password should be atleast of 6 characters',
     ),
   ]);
+
+  /// Submit
+  void _submit() {
+    bool checkUsername = _usernameValidator.isValid(_formData['username']);
+    bool checkEmail = _emailValidator.isValid(_formData['email']);
+    bool checkPassword = _passwordValidator.isValid(_formData['password']);
+
+    if (!checkUsername)
+      _invalidSnackBarMsg('Invalid username');
+    else if (!checkEmail)
+      _invalidSnackBarMsg('Invalid email');
+    else if (!checkPassword) _invalidSnackBarMsg('Invalid password');
+  }
+
+  void _invalidSnackBarMsg(String text) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        text,
+        style: Theme.of(context)
+            .textTheme
+            .bodyText1
+            ?.copyWith(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,6 +93,49 @@ class _SignupState extends State<Signup> {
               _passwordValidator,
               _formData,
               obscureText: true,
+            ),
+            SizedBox(height: 32),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 16),
+                    blurRadius: 32,
+                    color: Color(0xffFFC21E).withOpacity(0.15),
+                  ),
+                  BoxShadow(
+                    offset: Offset(0, -8),
+                    blurRadius: 16,
+                    color: Color(0xffFFC21E).withOpacity(0.05),
+                  ),
+                ],
+              ),
+              child: TextButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).accentColor,
+                  ),
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(vertical: 20, horizontal: 56),
+                  ),
+                ),
+                onPressed: () => _submit(),
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Sofia Pro',
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 2,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
