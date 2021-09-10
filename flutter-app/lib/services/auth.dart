7ffac1b782aac56b860e5e6c663b13ebe5bb2226
AkashSDas <aakashdas368@gmail.com>
2021-09-10 19:51:24 +0530

    refaactor signup function and use runAsync

--- flutter-app/lib/services/auth.dart
+++ flutter-app/lib/services/auth.dart
@@ -1,16 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:salt/utils.dart';
 
-void signup(Map<String, String> data) async {
-  try {
-    var response = await Dio().post(
-      'http://localhost:8000/api/main-user-auth/signup',
-      data: data,
-      options: Options(validateStatus: (int? status) {
-        return status! < 500;
-      }),
-    );
-    print(response);
-  } catch (err) {
-    print(err);
+Future<List<dynamic>> signup(Map<String, String> data) async {
+  final result = await runAsync(Dio().post(
+    'http://localhost:8000/api/main-user-auth/signup',
+    data: data,
+    options: Options(validateStatus: (int? status) {
+      return status! < 500;
+    }),
+  ));
+  if (result[0] != null) {
+    /// Getting response data
+    Response<dynamic> res = result[0] as Response<dynamic>;
+    result[0] = res.data;
   }
+  return result;
 }
