1463eaf5ec3bbab5e955ec82d953ec6671f172e1
AkashSDas <aakashdas368@gmail.com>
2021-09-10 20:25:25 +0530

    create login auth service

--- flutter-app/lib/services/auth.dart
+++ flutter-app/lib/services/auth.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'
+    as SecureStorage;
 import 'package:salt/utils.dart';
 
 Future<List<dynamic>> signup(Map<String, String> data) async {
@@ -14,5 +18,37 @@ Future<List<dynamic>> signup(Map<String, String> data) async {
     Response<dynamic> res = result[0] as Response<dynamic>;
     result[0] = res.data;
   }
+  return result;
+}
+
+Future<List<dynamic>> login(Map<String, String> data) async {
+  final result = await runAsync(Dio().post(
+    'http://localhost:8000/api/main-user-auth/login',
+    data: data,
+    options: Options(validateStatus: (int? status) {
+      return status! < 500;
+    }),
+  ));
+
+  if (result[0] != null) {
+    Response<dynamic> res = result[0] as Response<dynamic>;
+    result[0] = res.data;
+
+    if (!result[0]['error']) {
+      /// Save user data to secure storage
+      final _storage = SecureStorage.FlutterSecureStorage();
+      final response = await runAsync(
+        _storage.write(key: 'user', value: jsonEncode(res.data['data'])),
+      );
+
+      /// Couldn't save user info
+      if (response[1] == null) {
+        /// Making error true and response null
+        result[1] = true;
+        result[0] = null;
+      }
+    }
+  }
+
   return result;
 }
