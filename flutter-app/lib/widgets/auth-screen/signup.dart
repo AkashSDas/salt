d28acf3fb70aad28b652140b30e96032f26a0cba
AkashSDas <aakashdas368@gmail.com>
2021-09-10 13:16:08 +0530

    create form fields in signup widget

--- flutter-app/lib/widgets/auth-screen/signup.dart
+++ flutter-app/lib/widgets/auth-screen/signup.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:salt/widgets/form/input.dart';
 
 class Signup extends StatefulWidget {
   const Signup({Key? key}) : super(key: key);
@@ -11,19 +14,58 @@ class _SignupState extends State<Signup> {
   final _formKey = GlobalKey<FormState>();
   Map<String, String> _formData = {'email': '', 'username': '', 'password': ''};
 
+  final _usernameValidator = MultiValidator([
+    RequiredValidator(errorText: 'Username is required'),
+    MinLengthValidator(
+      3,
+      errorText: 'Username must be at least 3 characters long',
+    ),
+  ]);
+
+  final _emailValidator = MultiValidator([
+    RequiredValidator(errorText: 'Email is required'),
+    EmailValidator(errorText: 'Enter a valid email address'),
+  ]);
+
+  final _passwordValidator = MultiValidator([
+    MinLengthValidator(
+      6,
+      errorText: 'Password should be atleast of 6 characters',
+    ),
+  ]);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 4),
+      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 8),
       child: Form(
         key: _formKey,
         child: ListView(
           children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'John Smith',
-                labelText: 'Username',
-              ),
+            FormInputField(
+              'username',
+              'John Smith',
+              'Username',
+              _usernameValidator,
+              _formData,
+            ),
+            SizedBox(height: 32),
+            FormInputField(
+              'email',
+              'john@gmail.com',
+              'Email',
+              _emailValidator,
+              _formData,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 32),
+            FormInputField(
+              'password',
+              'secure password',
+              'Password',
+              _passwordValidator,
+              _formData,
+              obscureText: true,
             ),
           ],
         ),
