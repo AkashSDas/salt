033c0b5998910f1faa2125737337bd4de1989745
AkashSDas <aakashdas368@gmail.com>
2021-09-11 10:15:57 +0530

    create food category model

---
+++ flutter-app/lib/models/food-category/food-category.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'food-category.g.dart';
+
+@JsonSerializable()
+class FoodCategory {
+  final String emoji;
+  final String name;
+  final String description;
+
+  FoodCategory({
+    required this.emoji,
+    required this.name,
+    required this.description,
+  });
+
+  factory FoodCategory.fromJson(Map<String, dynamic> json) {
+    return _$FoodCategoryFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$FoodCategoryToJson(this);
+}
---
+++ flutter-app/lib/models/food-category/food-category.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'food-category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FoodCategory _$FoodCategoryFromJson(Map<String, dynamic> json) {
+  return FoodCategory(
+    emoji: json['emoji'] as String,
+    name: json['name'] as String,
+    description: json['description'] as String,
+  );
+}
+
+Map<String, dynamic> _$FoodCategoryToJson(FoodCategory instance) =>
+    <String, dynamic>{
+      'emoji': instance.emoji,
+      'name': instance.name,
+      'description': instance.description,
+    };
