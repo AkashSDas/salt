4d89e7756437d77ea91599644101d1f2be289e7d
AkashSDas <aakashdas368@gmail.com>
2021-09-11 20:12:27 +0530

    create user model

---
+++ flutter-app/lib/models/user/user.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) {
+  return User(
+    username: json['username'] as String,
+    email: json['email'] as String,
+    profilePicURL: json['profilePicURL'] as String,
+    role: json['role'] as int,
+  );
+}
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'username': instance.username,
+      'email': instance.email,
+      'profilePicURL': instance.profilePicURL,
+      'role': instance.role,
+    };
