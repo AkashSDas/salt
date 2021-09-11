865d5ed54fb1c106ec07a4881e07f91a33e067f3
AkashSDas <aakashdas368@gmail.com>
2021-09-11 19:53:30 +0530

    translate and animate body body depending on drawer open state

--- flutter-app/lib/screens/home.dart
+++ flutter-app/lib/screens/home.dart
@@ -3,9 +3,16 @@ import 'package:salt/widgets/common/app-bar.dart';
 import 'package:salt/widgets/common/drawer.dart';
 import 'package:salt/widgets/food-categories/categories-list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   /// Using a GlobalKey for the Custom Drawer to work
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  bool _isDrawerOpen = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +23,38 @@ class HomeScreen extends StatelessWidget {
         appBar: CustomAppBar(scaffoldKey: _scaffoldKey),
         drawer: AppDrawer(),
         drawerScrimColor: Colors.transparent,
+        onDrawerChanged: (isOpened) {
+          setState(() {
+            _isDrawerOpen = isOpened ? true : false;
+          });
+        },
         body: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              CategoriesList(),
-            ],
+          child: Transform.translate(
+            offset: _isDrawerOpen
+                ? Offset(MediaQuery.of(context).size.width * 0.59 + 32, 32)
+                : Offset(0, 0),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(_isDrawerOpen ? 32 : 0),
+                color: Theme.of(context).primaryColor,
+                boxShadow: _isDrawerOpen
+                    ? [
+                        BoxShadow(
+                          offset: Offset(-8, 0),
+                          blurRadius: 32,
+                          color: Color(0xffFFAE63).withOpacity(0.18),
+                        ),
+                      ]
+                    : null,
+              ),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  CategoriesList(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
