ecc076cce8431a248857e428d1d8bf78b75a6122
AkashSDas <aakashdas368@gmail.com>
2021-09-11 18:12:42 +0530

    add drawer in home screen

--- flutter-app/lib/screens/home.dart
+++ flutter-app/lib/screens/home.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:salt/widgets/common/app-bar.dart';
+import 'package:salt/widgets/common/drawer.dart';
 import 'package:salt/widgets/food-categories/categories-list.dart';
 
 class HomeScreen extends StatelessWidget {
+  /// Using a GlobalKey for the Custom Drawer to work
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        key: _scaffoldKey,
         backgroundColor: Theme.of(context).primaryColor,
-        appBar: CustomAppBar(),
+        appBar: CustomAppBar(scaffoldKey: _scaffoldKey),
+        drawer: AppDrawer(),
         body: Container(
           padding: EdgeInsets.all(16),
           child: Column(
