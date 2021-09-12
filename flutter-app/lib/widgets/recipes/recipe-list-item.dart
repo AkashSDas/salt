d50cef84603db9ccde6131ed8b19bf15034b3c59
AkashSDas <aakashdas368@gmail.com>
2021-09-12 14:07:34 +0530

    create img filter and text clamping in recipe list item widget

--- flutter-app/lib/widgets/recipes/recipe-list-item.dart
+++ flutter-app/lib/widgets/recipes/recipe-list-item.dart
@@ -12,29 +12,50 @@ class RecipeListItem extends StatelessWidget {
       margin: EdgeInsets.only(right: 24),
       width: 175,
       height: 225,
-      child: Positioned(
-        bottom: 0,
-        child: Container(
-          padding: EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: DesignSystem.grey1,
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: NetworkImage(recipe.coverImgURL),
-              fit: BoxFit.cover,
-            ),
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [Colors.black, Colors.transparent],
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: DesignSystem.grey1,
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: NetworkImage(recipe.coverImgURL),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          child: Container(
-            alignment: Alignment.bottomLeft,
-            child: Text(recipe.title),
-          ),
-        ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [Colors.black, Colors.transparent],
+              ),
+            ),
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 8,
+              ),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                clampText(recipe.title),
+                style: Theme.of(context).textTheme.caption?.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  String clampText(String text) {
+    int upto = 35;
+    if (text.length > upto) return text.substring(0, upto) + '...';
+    return text;
   }
 }
