8b3a0a9d778d5bee87bceab5242caacea6ea4694
AkashSDas <aakashdas368@gmail.com>
2021-09-12 19:11:23 +0530

    wrap material app with changenotifierprovider for user provider

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 import 'package:salt/designs/designs.dart';
+import 'package:salt/providers/user.dart';
 import 'package:salt/screens/auth.dart';
 import 'package:salt/screens/home.dart';
 
@@ -17,15 +19,18 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Salt',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: DesignSystem.grey0,
-          accentColor: DesignSystem.orange,
-          textTheme: DesignSystem.textTheme,
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => UserProvider(),
+        child: MaterialApp(
+          title: 'Salt',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: DesignSystem.grey0,
+            accentColor: DesignSystem.orange,
+            textTheme: DesignSystem.textTheme,
+          ),
+          routes: _getRoutes(context),
         ),
-        routes: _getRoutes(context),
       );
 
   Map<String, Widget Function(BuildContext)> _getRoutes(BuildContext context) {
