9e3e58c6a41dbdecb530ac81d48775222cf523a5
AkashSDas <aakashdas368@gmail.com>
2021-09-12 13:36:02 +0530

    rm old home screen and create new one with animated drawer and app bar

--- flutter-app/lib/screens/home.dart
+++ flutter-app/lib/screens/home.dart
@@ -1,66 +1,135 @@
 import 'package:flutter/material.dart';
-import 'package:salt/widgets/common/app-bar.dart';
-import 'package:salt/widgets/common/drawer.dart';
+import 'package:salt/widgets/common/animated-drawer-app-bar.dart';
 import 'package:salt/widgets/food-categories/categories-list.dart';
 import 'package:salt/widgets/recipes/recipes-list.dart';
 
+/// See experiment3.dart in widget to understand the
+/// working of appbar and drawer
+
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  /// Using a GlobalKey for the Custom Drawer to work
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  bool isDrawerOpen = false;
+  late AnimationController _drawerCtrl;
+  late AnimationController _bodyCtrl;
+  int animationDuration = 200;
+  final double appBarHeight = 64;
 
-  bool _isDrawerOpen = false;
+  void toggleDrawerState() => setState(() => isDrawerOpen != isDrawerOpen);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _drawerCtrl = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: animationDuration),
+      value: 1,
+    );
+
+    _bodyCtrl = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: animationDuration),
+      value: 0,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        key: _scaffoldKey,
         backgroundColor: Theme.of(context).primaryColor,
-        appBar: CustomAppBar(scaffoldKey: _scaffoldKey),
-        drawer: AppDrawer(),
-        drawerScrimColor: Colors.transparent,
-        onDrawerChanged: (isOpened) {
-          setState(() {
-            _isDrawerOpen = isOpened ? true : false;
-          });
+        appBar: CustomAppBar(
+          isDrawerOpen: isDrawerOpen,
+          drawerCtrl: _drawerCtrl,
+          bodyCtrl: _bodyCtrl,
+          toggleDrawerState: toggleDrawerState,
+        ),
+        body: Stack(
+          children: [
+            _buildDrawer(context),
+            _buildBody(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _drawerCtrl,
+      builder: (context, child) => Transform.translate(
+        offset: Offset(
+          -MediaQuery.of(context).size.width * 0.5 * _drawerCtrl.value,
+          0,
+        ),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.5,
+          color: Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _bodyCtrl,
+      builder: (context, child) => GestureDetector(
+        onTap: () {
+          if (isDrawerOpen) {
+            setState(() {
+              isDrawerOpen = !isDrawerOpen;
+            });
+          }
+          _drawerCtrl.forward();
+          _bodyCtrl.reverse();
         },
-        body: Container(
-          child: Transform.translate(
-            offset: _isDrawerOpen
-                ? Offset(MediaQuery.of(context).size.width * 0.59 + 32, 32)
-                : Offset(0, 0),
-            child: AnimatedContainer(
-              duration: Duration(milliseconds: 500),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(_isDrawerOpen ? 32 : 0),
-                color: Theme.of(context).primaryColor,
-                boxShadow: _isDrawerOpen
-                    ? [
-                        BoxShadow(
-                          offset: Offset(-8, 0),
-                          blurRadius: 32,
-                          color: Color(0xffFFAE63).withOpacity(0.18),
-                        ),
-                      ]
-                    : null,
-              ),
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  CategoriesList(),
-                  SizedBox(height: 16),
-                  RecipesList(),
-                ],
-              ),
+        child: Transform.translate(
+          offset: Offset(
+            MediaQuery.of(context).size.width * 0.6 * _bodyCtrl.value,
+            128 * _bodyCtrl.value,
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 16 + 16 * _bodyCtrl.value,
+              horizontal: 16 + 8 * _bodyCtrl.value,
             ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(-16 * _bodyCtrl.value, 0),
+                  blurRadius: 64 * _bodyCtrl.value,
+                  color: Colors.orange.withOpacity(0.25),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(64 * _bodyCtrl.value),
+            ),
+            child: _Body(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      clipBehavior: Clip.none,
+      children: [
+        CategoriesList(),
+        SizedBox(height: 32),
+        RecipesList(),
+      ],
     );
   }
 }
