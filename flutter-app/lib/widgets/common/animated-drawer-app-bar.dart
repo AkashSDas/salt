e192d5bd70b9363c5308aa477edfc768957cb400
AkashSDas <aakashdas368@gmail.com>
2021-09-12 13:26:37 +0530

    remove appbar and create appbar for animated drawer

---
+++ flutter-app/lib/widgets/common/animated-drawer-app-bar.dart
@@ -0,0 +1,90 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  bool isDrawerOpen;
+  AnimationController drawerCtrl;
+  AnimationController bodyCtrl;
+  Function toggleDrawerState;
+
+  final double appBarHeight = 64;
+
+  CustomAppBar({
+    required this.isDrawerOpen,
+    required this.drawerCtrl,
+    required this.bodyCtrl,
+    required this.toggleDrawerState,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+
+  @override
+  _CustomAppBarState createState() => _CustomAppBarState();
+}
+
+class _CustomAppBarState extends State<CustomAppBar>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: widget.drawerCtrl,
+      builder: (context, child) => Container(
+        alignment: Alignment.bottomCenter,
+        color: Theme.of(context).primaryColor,
+        height: widget.appBarHeight * widget.drawerCtrl.value,
+        child: Container(
+          height: widget.appBarHeight,
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildDrawerBtn(context),
+              _buildPlaceholderBtn(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawerBtn(BuildContext context) => IconButton(
+        icon: AspectRatio(
+          aspectRatio: 1,
+          child: FlareActor(
+            'assets/flare/icons/filter.flr',
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            animation: 'idle',
+          ),
+        ),
+        color: Colors.black,
+        onPressed: () {
+          /// Any changes happening to widget.isDrawerOpen won't be reflected in Screen widget
+          /// and any change happening in Screen widget won't be reflected here. This is
+          /// because no global state is used (provider, etc can be used to achieve it)
+          /// That's why widget.toggleDrawerState() (will update Screen widget isDrawerOpen value)
+          /// and setState for widget.isDrawerOpen (will update widget.isDrawerOpen and not Screen
+          /// widget's isDrawerOpen state) are called here together
+
+          widget.toggleDrawerState();
+          setState(() {
+            widget.isDrawerOpen = !widget.isDrawerOpen;
+          });
+          if (widget.isDrawerOpen) {
+            widget.drawerCtrl.reverse();
+            widget.bodyCtrl.forward();
+          } else {
+            widget.drawerCtrl.forward();
+            widget.bodyCtrl.reverse();
+          }
+        },
+      );
+
+  Widget _buildPlaceholderBtn(BuildContext context) => IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        color: Colors.transparent,
+        onPressed: () {},
+      );
+}
