666cd26fe451966f60bba228eb6c9e90c60b2515
AkashSDas <aakashdas368@gmail.com>
2021-09-15 19:05:47 +0530

    create blog post model

---
+++ flutter-app/lib/models/blog-post/blog-post.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:salt/models/food-category/food-category.dart';
+import 'package:salt/models/user/user.dart';
+
+part 'blog-post.g.dart';
+
+@JsonSerializable()
+class BlogPost {
+  final String title;
+  final String description;
+  final String content;
+  final String coverImgURL;
+  final double readTime;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final List<FoodCategory> categories;
+  final User author;
+
+  BlogPost({
+    required this.title,
+    required this.description,
+    required this.content,
+    required this.coverImgURL,
+    required this.readTime,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.categories,
+    required this.author,
+  });
+
+  factory BlogPost.fromJson(Map<String, dynamic> json) {
+    return _$BlogPostFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$BlogPostToJson(this);
+}
---
+++ flutter-app/lib/models/blog-post/blog-post.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'blog-post.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BlogPost _$BlogPostFromJson(Map<String, dynamic> json) {
+  return BlogPost(
+    title: json['title'] as String,
+    description: json['description'] as String,
+    content: json['content'] as String,
+    coverImgURL: json['coverImgURL'] as String,
+    readTime: (json['readTime'] as num).toDouble(),
+    createdAt: DateTime.parse(json['createdAt'] as String),
+    updatedAt: DateTime.parse(json['updatedAt'] as String),
+    categories: (json['categories'] as List<dynamic>)
+        .map((e) => FoodCategory.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    author: User.fromJson(json['author'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$BlogPostToJson(BlogPost instance) => <String, dynamic>{
+      'title': instance.title,
+      'description': instance.description,
+      'content': instance.content,
+      'coverImgURL': instance.coverImgURL,
+      'readTime': instance.readTime,
+      'createdAt': instance.createdAt.toIso8601String(),
+      'updatedAt': instance.updatedAt.toIso8601String(),
+      'categories': instance.categories,
+      'author': instance.author,
+    };
