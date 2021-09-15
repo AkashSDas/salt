9dacc66c7dbc5d8b46617cdef2d584fc825c2b1a
AkashSDas <aakashdas368@gmail.com>
2021-09-15 21:37:12 +0530

    create service for retrieving all blog posts

---
+++ flutter-app/lib/services/blog-post.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:salt/models/blog-post/blog-post.dart';
+import 'package:salt/utils.dart';
+
+Future<dynamic> getAllBlogPostsPaginated({int? limit}) async {
+  limit = limit != null ? limit : 10;
+  var response = await runAsync(
+    Dio().get(
+      '${dotenv.env["BACKEND_API_BASE_URL"]}blog-post?limit=$limit',
+      options: Options(validateStatus: (int? status) {
+        return status! < 500;
+      }),
+    ),
+  );
+
+  if (response[0] != null) {
+    Response<dynamic> res = response[0] as Response<dynamic>;
+    response[0] = res.data;
+
+    if (!response[0]['error']) {
+      List<dynamic> posts = response[0]['data']['posts'];
+      for (int i = 0; i < posts.length; i++) {
+        posts[i] = BlogPost.fromJson(posts[i]);
+      }
+      response[0]['data']['posts'] = posts;
+    }
+  }
+
+  return response;
+}
