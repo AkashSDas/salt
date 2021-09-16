401342f56fbabd72e37f7e1446e2df7e846dbf0b
AkashSDas <aakashdas368@gmail.com>
2021-09-16 14:40:53 +0530

    rm old way and use blog post list item widget to display posts

--- flutter-app/lib/widgets/blog-post/blog-post-list.dart
+++ flutter-app/lib/widgets/blog-post/blog-post-list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:salt/designs/designs.dart';
 import 'package:salt/services/blog-post.dart';
 import 'package:salt/widgets/blog-post/blog-post-list-item-loader.dart';
+import 'package:salt/widgets/blog-post/blog-post-list-item.dart';
 
 class BlogPostList extends StatefulWidget {
   const BlogPostList({Key? key}) : super(key: key);
@@ -35,72 +36,7 @@ class _BlogPostListState extends State<BlogPostList> {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ...posts
-                .map(
-                  (post) => Container(
-                    color: Theme.of(context).primaryColor,
-                    margin: EdgeInsets.only(bottom: 16),
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 16 * 10,
-                          decoration: BoxDecoration(
-                            color: DesignSystem.grey1,
-                            image: DecorationImage(
-                              image: NetworkImage(post.coverImgURL),
-                              fit: BoxFit.cover,
-                            ),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          post.title,
-                          style:
-                              Theme.of(context).textTheme.bodyText1?.copyWith(
-                                    fontWeight: FontWeight.w700,
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .headline1
-                                        ?.color,
-                                  ),
-                          textAlign: TextAlign.left,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              height: 38,
-                              width: 38,
-                              margin: EdgeInsets.only(right: 8),
-                              decoration: BoxDecoration(
-                                color: DesignSystem.grey1,
-                                borderRadius: BorderRadius.circular(12),
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                    post.author.profilePicURL,
-                                  ),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Column(
-                              children: [
-                                Text(post.author.username),
-                                Text(
-                                  '${post.updatedAt} - ${post.readTime}min read',
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-                .toList(),
+            ...posts.map((post) => BlogPostListItem(post: post)).toList(),
           ],
         );
       },
