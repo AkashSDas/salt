d7b02b98e4a4adadf5b7d3a02079f64e9e70521d
AkashSDas <aakashdas368@gmail.com>
2021-09-17 18:36:22 +0530

    create logout service

--- flutter-app/lib/services/auth.dart
+++ flutter-app/lib/services/auth.dart
@@ -66,3 +66,25 @@ Future<dynamic> isAuthenticated() async {
     return response[0];
   }
 }
+
+Future<dynamic> logout() async {
+  var result = await runAsync(Dio().get(
+    '${dotenv.env["BACKEND_API_BASE_URL"]}main-user-auth/logout',
+    options: Options(validateStatus: (int? status) {
+      return status! < 500;
+    }),
+  ));
+
+  if (result[1] == null) {
+    Response<dynamic> res = result[0] as Response<dynamic>;
+    result[0] = res.data;
+
+    if (!result[0]['error']) {
+      /// Logout user in this device
+      final _storage = SecureStorage.FlutterSecureStorage();
+      await runAsync(_storage.delete(key: 'user'));
+    }
+  }
+
+  return result;
+}
