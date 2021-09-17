a54eb6b6bf14df3d5383b9c69cc9912bf6be8847
AkashSDas <aakashdas368@gmail.com>
2021-09-17 09:04:22 +0530

    make get all blog post service able to handle pagination request

--- flutter-app/lib/services/blog-post.dart
+++ flutter-app/lib/services/blog-post.dart
@@ -3,11 +3,22 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:salt/models/blog-post/blog-post.dart';
 import 'package:salt/utils.dart';
 
-Future<dynamic> getAllBlogPostsPaginated({int? limit}) async {
+Future<dynamic> getAllBlogPostsPaginated({
+  int? limit,
+  bool? hasNext,
+  String? nextId,
+}) async {
   limit = limit != null ? limit : 10;
+
+  String baseURL =
+      '${dotenv.env["BACKEND_API_BASE_URL"]}blog-post?limit=$limit';
+  if (hasNext == true) {
+    baseURL = '$baseURL&next=$nextId';
+  }
+
   var response = await runAsync(
     Dio().get(
-      '${dotenv.env["BACKEND_API_BASE_URL"]}blog-post?limit=$limit',
+      baseURL,
       options: Options(validateStatus: (int? status) {
         return status! < 500;
       }),
