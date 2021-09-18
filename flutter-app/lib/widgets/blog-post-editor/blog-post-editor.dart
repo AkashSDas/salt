2a73a926880993f541c4cb9ab53007b621e675b7
AkashSDas <aakashdas368@gmail.com>
2021-09-18 21:08:35 +0530

    refactor blog post editor and use food categories tags

--- flutter-app/lib/widgets/blog-post-editor/blog-post-editor.dart
+++ flutter-app/lib/widgets/blog-post-editor/blog-post-editor.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:provider/provider.dart';
-import 'package:salt/designs/designs.dart';
-import 'package:salt/models/food-category/food-category.dart';
 import 'package:salt/providers/food-categories.dart';
-import 'package:salt/services/food-categories.dart';
 import 'package:salt/widgets/blog-post-editor/description-input.dart';
 import 'package:salt/widgets/blog-post-editor/food-categories-dropdown.dart';
+import 'package:salt/widgets/blog-post-editor/food-categories-tags.dart';
 import 'package:salt/widgets/blog-post-editor/title-input.dart';
 
 class BlogPostEditor extends StatefulWidget {
@@ -18,37 +16,6 @@ class BlogPostEditor extends StatefulWidget {
 
 class _BlogPostEditorState extends State<BlogPostEditor> {
   Map<String, String> _formData = {'title': '', 'description': ''};
-  List<dynamic> _categories = []; // List<FoodCategory>
-  FoodCategory _foodCategoryState = FoodCategory(
-    id: 'default',
-    emoji: '',
-    name: 'Select tags',
-    description: 'Default dropdown value',
-  );
-
-  List<FoodCategory> _tags = [];
-
-  bool categoriesLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _getFoodCategories();
-  }
-
-  Future<void> _getFoodCategories() async {
-    setState(() {
-      categoriesLoading = true;
-    });
-    var response = await getAllFoodCategories();
-    if (response[1] == null) {
-      /// No error
-      setState(() {
-        _categories = response[0]['data']['categories'];
-        categoriesLoading = false;
-      });
-    }
-  }
 
   final _titleValidator = MultiValidator([
     RequiredValidator(errorText: 'Title is required'),
@@ -95,74 +62,7 @@ class _BlogPostEditorState extends State<BlogPostEditor> {
                 SizedBox(height: 16),
                 FoodCategoriesDropDown(),
                 SizedBox(height: 16),
-                Wrap(
-                  spacing: 8,
-                  runSpacing: 8,
-                  children: List.generate(
-                    _tags.length,
-                    (index) => Container(
-                      height: 44,
-                      padding:
-                          EdgeInsets.only(top: 6, bottom: 6, left: 8, right: 8),
-                      decoration: BoxDecoration(
-                        border:
-                            Border.all(color: Color(0xffBDBDBD), width: 0.5),
-                        borderRadius: BorderRadius.circular(21),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                            height: 36,
-                            width: 36,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              boxShadow: [
-                                BoxShadow(
-                                  offset: Offset(0, 0),
-                                  blurRadius: 4,
-                                  color: Colors.black.withOpacity(0.15),
-                                ),
-                              ],
-                              color: Theme.of(context).primaryColor,
-                            ),
-                            child: Center(child: Text(_tags[index].emoji)),
-                          ),
-                          SizedBox(width: 8),
-                          Text(
-                            '${_tags[index].name[0].toUpperCase()}${_tags[index].name.substring(1)}',
-                            style: Theme.of(context).textTheme.caption,
-                          ),
-                          SizedBox(width: 8),
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _categories = [..._categories, _tags[index]];
-                                _tags = _tags
-                                    .where((tag) => tag.id != _tags[index].id)
-                                    .toList();
-                              });
-                            },
-                            child: Container(
-                              height: 36,
-                              width: 36,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: DesignSystem.grey2,
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  Icons.cancel,
-                                  color: DesignSystem.grey3,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                FoodCategoriesTags(),
               ],
             ),
           ),
