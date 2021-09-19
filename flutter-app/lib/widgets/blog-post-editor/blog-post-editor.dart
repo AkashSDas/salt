8300db7a1f0812a4db64fd5ecfb54074c49737aa
AkashSDas <aakashdas368@gmail.com>
2021-09-19 12:07:22 +0530

    create image uploading feature and use blog editor in blog post editor widget

--- flutter-app/lib/widgets/blog-post-editor/blog-post-editor.dart
+++ flutter-app/lib/widgets/blog-post-editor/blog-post-editor.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:salt/designs/designs.dart';
 import 'package:salt/providers/food-categories.dart';
 import 'package:salt/widgets/blog-post-editor/description-input.dart';
+import 'package:salt/widgets/blog-post-editor/editor.dart';
 import 'package:salt/widgets/blog-post-editor/food-categories-dropdown.dart';
 import 'package:salt/widgets/blog-post-editor/food-categories-tags.dart';
 import 'package:salt/widgets/blog-post-editor/title-input.dart';
@@ -15,7 +21,16 @@ class BlogPostEditor extends StatefulWidget {
 }
 
 class _BlogPostEditorState extends State<BlogPostEditor> {
-  Map<String, String> _formData = {'title': '', 'description': ''};
+  Map<String, String> _formData = {
+    'title': '',
+    'description': '',
+    'content': ''
+  };
+
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? _imageFiles;
+
+  bool preview = false;
 
   final _titleValidator = MultiValidator([
     RequiredValidator(errorText: 'Title is required'),
@@ -30,6 +45,14 @@ class _BlogPostEditorState extends State<BlogPostEditor> {
     MinLengthValidator(
       6,
       errorText: 'Description should be atleast 6 characters long',
+    ),
+  ]);
+
+  final _contentValidator = MultiValidator([
+    RequiredValidator(errorText: 'Content is required'),
+    MinLengthValidator(
+      6,
+      errorText: 'Content should be atleast 6 characters long',
     ),
   ]);
 
@@ -63,6 +86,114 @@ class _BlogPostEditorState extends State<BlogPostEditor> {
                 FoodCategoriesDropDown(),
                 SizedBox(height: 16),
                 FoodCategoriesTags(),
+                SizedBox(height: 16),
+
+                /// Wrapping text button with column to avoid text button
+                /// taking entire width and then fixing the its width with padding
+                /// and content width
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        try {
+                          final pickedFile = await _picker.pickImage(
+                            source: ImageSource.gallery,
+                          );
+
+                          if (pickedFile != null)
+                            setState(() {
+                              _imageFiles = [pickedFile];
+                            });
+                        } catch (err) {}
+                      },
+                      child: Text(
+                        'Choose cover image',
+                        style: TextStyle(
+                          color: DesignSystem.grey4,
+                          fontFamily: 'Sofia Pro',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).accentColor.withOpacity(0.6),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                _imageFiles != null
+                    ? Container(
+                        height: 16 * 10,
+                        decoration: BoxDecoration(
+                          color: DesignSystem.grey2,
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                            image: Image.file(
+                              File(_imageFiles![0].path),
+                            ).image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : SizedBox(),
+
+                SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          preview = !preview;
+                        });
+                      },
+                      child: Text(
+                        'Preview',
+                        style: TextStyle(
+                          color: DesignSystem.grey4,
+                          fontFamily: 'Sofia Pro',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).accentColor.withOpacity(0.6),
+                        ),
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                preview
+                    ? Markdown(data: _formData['content'] ?? '')
+                    : BlogEditor(
+                        name: 'content',
+                        label: 'Content',
+                        hintText: 'Amazing content goes here',
+                        formData: _formData,
+                        validator: _contentValidator,
+                      ),
               ],
             ),
           ),
