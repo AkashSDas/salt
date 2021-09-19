ee85eca909d36823f36c2f4b22dd2284df94b664
AkashSDas <aakashdas368@gmail.com>
2021-09-19 19:16:09 +0530

    add text btn for save blog post and refactor blog post editor

--- flutter-app/lib/widgets/blog-post-editor/blog-post-editor.dart
+++ flutter-app/lib/widgets/blog-post-editor/blog-post-editor.dart
@@ -8,6 +8,8 @@ import 'package:provider/provider.dart';
 import 'package:salt/designs/designs.dart';
 import 'package:salt/providers/blog-post-editor.dart';
 import 'package:salt/providers/food-categories.dart';
+import 'package:salt/providers/user.dart';
+import 'package:salt/services/blog-post.dart';
 import 'package:salt/widgets/blog-post-editor/description-input.dart';
 import 'package:salt/widgets/blog-post-editor/editor.dart';
 import 'package:salt/widgets/blog-post-editor/food-categories-dropdown.dart';
@@ -24,140 +26,185 @@ class BlogPostEditor extends StatefulWidget {
 
 class _BlogPostEditorState extends State<BlogPostEditor> {
   final ImagePicker _picker = ImagePicker();
-  List<XFile>? _imageFiles;
+  List<XFile> _imageFiles = [];
 
   bool preview = false;
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FoodCategoriesProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => BlogPostEditorProvider(),
-        ),
-      ],
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(),
-          body: Padding(
-            padding: EdgeInsets.all(16),
-            child: ListView(
-              children: [
-                TitleFormInput(),
-                SizedBox(height: 16),
-                DescriptionFormInput(),
-                SizedBox(height: 16),
-                FoodCategoriesDropDown(),
-                SizedBox(height: 16),
-                FoodCategoriesTags(),
-                SizedBox(height: 16),
+    BlogPostEditorProvider _provider = Provider.of<BlogPostEditorProvider>(
+      context,
+    );
+    FoodCategoriesProvider _fcProvider = Provider.of<FoodCategoriesProvider>(
+      context,
+    );
+    UserProvider _user = Provider.of<UserProvider>(
+      context,
+    );
 
-                /// Wrapping text button with column to avoid text button
-                /// taking entire width and then fixing the its width with padding
-                /// and content width
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () async {
-                        try {
-                          final pickedFile = await _picker.pickImage(
-                            source: ImageSource.gallery,
-                          );
+    return Padding(
+      padding: EdgeInsets.all(16).copyWith(bottom: 32),
+      child: ListView(
+        children: [
+          TitleFormInput(),
+          SizedBox(height: 16),
+          DescriptionFormInput(),
+          SizedBox(height: 16),
+          FoodCategoriesDropDown(),
+          SizedBox(height: 16),
+          FoodCategoriesTags(),
+          SizedBox(height: 16),
 
-                          if (pickedFile != null)
-                            setState(() {
-                              _imageFiles = [pickedFile];
-                            });
-                        } catch (err) {}
-                      },
-                      child: Text(
-                        'Choose cover image',
-                        style: TextStyle(
-                          color: DesignSystem.grey4,
-                          fontFamily: 'Sofia Pro',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15,
-                        ),
-                      ),
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).accentColor.withOpacity(0.6),
-                        ),
-                        padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                        ),
-                      ),
-                    ),
-                  ],
+          /// Wrapping text button with column to avoid text button
+          /// taking entire width and then fixing the its width with padding
+          /// and content width
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  try {
+                    final pickedFile = await _picker.pickImage(
+                      source: ImageSource.gallery,
+                    );
+
+                    if (pickedFile != null)
+                      setState(() {
+                        _imageFiles = [pickedFile];
+                      });
+                  } catch (err) {}
+                },
+                child: Text(
+                  'Choose cover image',
+                  style: TextStyle(
+                    color: DesignSystem.grey4,
+                    fontFamily: 'Sofia Pro',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                  ),
                 ),
-                SizedBox(height: 16),
-                _imageFiles != null
-                    ? Container(
-                        height: 16 * 10,
-                        decoration: BoxDecoration(
-                          color: DesignSystem.grey2,
-                          borderRadius: BorderRadius.circular(12),
-                          image: DecorationImage(
-                            image: Image.file(
-                              File(_imageFiles![0].path),
-                            ).image,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )
-                    : SizedBox(),
-
-                SizedBox(height: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          preview = !preview;
-                        });
-                      },
-                      child: Text(
-                        'Preview',
-                        style: TextStyle(
-                          color: DesignSystem.grey4,
-                          fontFamily: 'Sofia Pro',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15,
-                        ),
-                      ),
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).accentColor.withOpacity(0.6),
-                        ),
-                        padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                        ),
-                      ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                  ],
+                  ),
+                  backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).accentColor.withOpacity(0.6),
+                  ),
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  ),
                 ),
-                SizedBox(height: 16),
-                preview ? PreviewBlogPost() : BlogEditor(),
-                // BlogEditor(),
-              ],
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          _imageFiles.length > 0
+              ? Container(
+                  height: 16 * 10,
+                  decoration: BoxDecoration(
+                    color: DesignSystem.grey2,
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                      image: Image.file(
+                        File(_imageFiles[0].path),
+                      ).image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              : SizedBox(),
+
+          SizedBox(height: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    preview = !preview;
+                  });
+                },
+                child: Text(
+                  'Preview',
+                  style: TextStyle(
+                    color: DesignSystem.grey4,
+                    fontFamily: 'Sofia Pro',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                  ),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).accentColor.withOpacity(0.6),
+                  ),
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          preview ? PreviewBlogPost() : BlogEditor(),
+          SizedBox(height: 32),
+          TextButton(
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(
+                Theme.of(context).accentColor,
+              ),
+              padding: MaterialStateProperty.all(
+                EdgeInsets.symmetric(vertical: 20, horizontal: 56),
+              ),
+            ),
+            onPressed: () async {
+              if (_user.token == null) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                    'You must be logged in to upload your post ${_user.token}',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        ?.copyWith(color: Colors.white),
+                  ),
+                  backgroundColor: Colors.red,
+                ));
+              } else if (_imageFiles.length > 0) {
+                NewBlogPost post = NewBlogPost(
+                  title: _provider.title,
+                  description: _provider.description,
+                  content: _provider.content,
+                  categories: _fcProvider.getAllTagIds(),
+                  authorId: _user.user!.id,
+                  coverImg: _imageFiles[0],
+                );
+
+                await saveBlogPost(post, _user.token.toString());
+              }
+            },
+            child: Text(
+              'Save',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Sofia Pro',
+                fontWeight: FontWeight.w900,
+                letterSpacing: 2,
+                fontSize: 15,
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
