ad02792223fa12fc631ccdf8508551e102d3abad
AkashSDas <aakashdas368@gmail.com>
2021-09-19 19:11:56 +0530

    create save blog post service

--- flutter-app/lib/services/blog-post.dart
+++ flutter-app/lib/services/blog-post.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:salt/models/blog-post/blog-post.dart';
+import 'package:salt/models/user/user.dart';
 import 'package:salt/utils.dart';
 
 Future<dynamic> getAllBlogPostsPaginated({
@@ -36,6 +40,76 @@ Future<dynamic> getAllBlogPostsPaginated({
       }
       response[0]['data']['posts'] = posts;
     }
+  }
+
+  return response;
+}
+
+class NewBlogPost {
+  final String title;
+  final String description;
+  final String content;
+  final List<String> categories;
+  final String authorId;
+  final XFile coverImg;
+
+  const NewBlogPost({
+    required this.title,
+    required this.description,
+    required this.content,
+    required this.categories,
+    required this.authorId,
+    required this.coverImg,
+  });
+}
+
+Future<dynamic> saveBlogPost(NewBlogPost post, String token) async {
+  String baseURL = '${dotenv.env["BACKEND_API_BASE_URL"]}blog-post';
+
+  /// read time
+  int wordCount = post.content.trim().split(' ').length;
+  double readTime = (wordCount / 100 + 1).roundToDouble();
+
+  /// image
+  String filename = post.coverImg.path.split('/').last;
+
+  FormData formData = FormData.fromMap({
+    'title': post.title,
+    'description': post.description,
+    'content': post.content,
+    'readTime': readTime,
+    'author': post.authorId,
+    'coverImg': await MultipartFile.fromFile(
+      post.coverImg.path,
+      filename: filename,
+    ),
+    'categories': post.categories,
+  });
+
+  var response = await runAsync(
+    Dio().post(
+      '$baseURL/${post.authorId}',
+      data: formData,
+      options: Options(
+        validateStatus: (int? status) => status! < 500,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      ),
+    ),
+  );
+
+  print(response);
+
+  if (response[0] != null) {
+    Response<dynamic> res = response[0] as Response<dynamic>;
+    response[0] = res.data;
+
+    if (!response[0]['error']) {
+      var post = response[0]['data']['post'];
+      post = BlogPost.fromJson(post);
+    }
+    response[0]['data']['post'] = post;
   }
 
   return response;
