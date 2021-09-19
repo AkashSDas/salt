e960d992ae900fbc86359140284cffaa5d7c47ab
AkashSDas <aakashdas368@gmail.com>
2021-09-19 19:05:11 +0530

    update food category model

--- flutter-app/lib/models/food-category/food-category.dart
+++ flutter-app/lib/models/food-category/food-category.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'food-category.g.dart';
 
-// @JsonSerializable()
+@JsonSerializable()
 class FoodCategory {
   final String id;
   final String emoji;
--- flutter-app/lib/models/food-category/food-category.g.dart
+++ flutter-app/lib/models/food-category/food-category.g.dart
@@ -6,20 +6,6 @@ part of 'food-category.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-/// The code here is modified by `hand`
-/// as id field of FoodCategory will be populated from _id and not id
-/// similarly while converting to json id field is removed
-/// Note: after making these changes the JsonSerializable decorator
-/// from FoodCategory in 'food-category.dart' is removed so that builder
-/// doesn't re-generate this 'food-category.g.dart' file, overwritting the
-/// changes made here
-/// If you want to make any changes then first change FoodCategory and
-/// add JsonSerializable decorator on it and then see what changes are
-/// made in 'food-category.g.dart' by you and then run the builder and
-/// then remove JsonSerializable on FoodCategory and then make the changes
-/// that are were made in food-category.g.dart (OR just get the errors and
-/// resolve it)
-
 FoodCategory _$FoodCategoryFromJson(Map<String, dynamic> json) {
   return FoodCategory(
     id: json['_id'] as String,
