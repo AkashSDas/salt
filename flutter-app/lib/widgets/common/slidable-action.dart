951120af0951a4254cbdb7dbc511fc37314665a3
AkashSDas <aakashdas368@gmail.com>
2021-09-20 19:53:38 +0530

    wrap sliable action top widget with inkwell with on tap

--- flutter-app/lib/widgets/common/slidable-action.dart
+++ flutter-app/lib/widgets/common/slidable-action.dart
@@ -5,39 +5,44 @@ class SlidableAction extends StatelessWidget {
   final String flareAssetPath;
   final String label;
   final Color color;
+  final Function()? onTap;
 
   const SlidableAction({
     required this.flareAssetPath,
     required this.label,
     required this.color,
+    required this.onTap,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: color,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          FlareActor(
-            flareAssetPath,
-            sizeFromArtboard: true,
-            color: Colors.white,
-          ),
-          SizedBox(height: 16),
-          Text(
-            label,
-            style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-          ),
-        ],
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: color,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FlareActor(
+              flareAssetPath,
+              sizeFromArtboard: true,
+              color: Colors.white,
+            ),
+            SizedBox(height: 16),
+            Text(
+              label,
+              style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
