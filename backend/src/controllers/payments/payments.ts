313b1b18637983e29de2d25a9149d71013f821b3
AkashSDas <aakashdas368@gmail.com>
2021-09-23 12:13:35 +0530

    create create payment intent and charge function

---
+++ backend/src/controllers/payments/payments.ts
@@ -0,0 +1,23 @@
+import { stripe } from "../..";
+import { runAsync } from "../../utils";
+import { getOrCreateCustomer } from "./customers";
+
+/**
+ * For this the user has to be authenticated
+ */
+export async function createPaymentIntentAndCharge(
+  userId: string,
+  amount: number,
+  payment_method: string
+) {
+  const [customer, _err] = await runAsync(getOrCreateCustomer(userId));
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    customer,
+    payment_method,
+    currency: "inr",
+    off_session: true,
+    confirm: true,
+  });
+  return paymentIntent;
+}
