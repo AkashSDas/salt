3964a554d198241adf8f6558c2b6e6f091986928
AkashSDas <aakashdas368@gmail.com>
2021-09-23 14:25:09 +0530

    add more info in doc comment in order route

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -15,11 +15,14 @@ router.param("userId", getMainUserById);
 /// Routes
 
 /**
- * The flow for this request is that \
+ * The flow for this request is that
  * - User auth will be checked and if authenticated then
  * - Products will be pushed into user's purchases list
  * - Quantity left and sold for each product will be updated
  * - This order will be saved in the database
+ *
+ * Note: all of this operations can be done in stripe webhook
+ * after successfull payment (alternative approach)
  */
 router.post(
   "/",
