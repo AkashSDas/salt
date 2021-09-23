49ddbca691652d952060b0fc3846c731fdb79bda
AkashSDas <aakashdas368@gmail.com>
2021-09-23 11:40:48 +0530

    initialize stripe

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 // First loading env variables and then importing from other modules
 import { config } from "dotenv";
 import { connect } from "mongoose";
+import Stripe from "stripe";
 
 // Load env variables
 if (process.env.NODE_ENV !== "production") config();
@@ -13,6 +14,11 @@ connect(process.env.MONGODB_CONNECT_URL)
   .catch((err) =>
     console.log(`Cannot connect to MongoDB Atlas\nError: ${err}`)
   );
+
+// Initialize Stripe
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: "2020-08-27",
+});
 
 // Start the server
 const port = process.env.PORT || 8000;
