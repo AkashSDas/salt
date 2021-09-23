904431db4c77b0f3de8f983988778704d444c341
AkashSDas <aakashdas368@gmail.com>
2021-09-23 13:53:36 +0530

    create update product stock middleware

--- backend/src/controllers/product/middlewares.ts
+++ backend/src/controllers/product/middlewares.ts
@@ -21,3 +21,42 @@ export async function getProductById(
   req.product = product;
   next();
 }
+
+/// Update product stock whenever someone successfully pays for it
+async function updateProductStock(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  /// Going through each product that user has successfully payed for
+  /// in the order and creating updateOne operation for each where stock
+  /// is descreased and sold is increased
+  let operations = req.body.order.products.map((product) => {
+    return {
+      updateOne: {
+        filter: { _id: product._id },
+
+        /// increasing sold and decreasing stock
+        update: {
+          $inc: {
+            quantity_left: -product.quantity,
+            quantity_sold: +product.quantity,
+          },
+        },
+      },
+    };
+  });
+
+  /// Performing bulk write
+  const options = {};
+  const [_, err] = await runAsync(Product.bulkWrite(operations, options));
+  if (err)
+    return responseMsg(res, {
+      status: 400,
+      message: "Failed to update product stock",
+    });
+
+  next();
+}
+
+export default updateProductStock;
