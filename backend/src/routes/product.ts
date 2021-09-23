095b056acf3d840bb692d57806286bc59df6767b
AkashSDas <aakashdas368@gmail.com>
2021-09-23 09:18:43 +0530

    add route for product update

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import { getMainUserById } from "../controllers/main-user/middlewares";
 import createProduct from "../controllers/product/create";
+import updateProduct from "../controllers/product/update";
 
 export const router = Router();
 
@@ -10,3 +11,4 @@ router.param("userId", getMainUserById);
 
 /// Routes
 router.post("/:userId", isSignedIn, isAuthenticated, createProduct);
+router.put("/:productId/:userId", isSignedIn, isAuthenticated, updateProduct);
