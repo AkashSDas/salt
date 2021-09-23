fcaaf821ad8b31eea2dda38f63f62f32faca0660
AkashSDas <aakashdas368@gmail.com>
2021-09-23 09:28:37 +0530

    create router param from getting product by id

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { isAuthenticated, isSignedIn } from "../controllers/auth/middlewares";
 import { getMainUserById } from "../controllers/main-user/middlewares";
 import createProduct from "../controllers/product/create";
+import { getProductById } from "../controllers/product/middlewares";
 import updateProduct from "../controllers/product/update";
 
 export const router = Router();
 
 /// Params
 router.param("userId", getMainUserById);
+router.param("productId", getProductById);
 
 /// Routes
 router.post("/:userId", isSignedIn, isAuthenticated, createProduct);
