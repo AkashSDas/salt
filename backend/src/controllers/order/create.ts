605f83b5bfc6c4b0b05543a4eb1764003d77029c
AkashSDas <aakashdas368@gmail.com>
2021-09-23 14:19:59 +0530

    add doc comment on create order

--- backend/src/controllers/order/create.ts
+++ backend/src/controllers/order/create.ts
@@ -3,6 +3,14 @@ import { Order } from "../../models/order";
 import { runAsync } from "../../utils";
 import { responseMsg } from "../json-response";
 
+/**
+ * Here we'll create order after successfull payment of the purchase
+ * whose order we're going to make here. This request should be made
+ * just after successfull payment of the user's purchase
+ *
+ * User has to be authenticated to make this request
+ * Here Request.body.order will be of shape orderSchema
+ */
 async function createOrder(req: Request, res: Response) {
   /// Get the user making order
   req.body.order.user = req.profile;
