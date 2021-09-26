8641e651c89c5becf641ce2f4d6d8d8528fbc5b3
AkashSDas <aakashdas368@gmail.com>
2021-09-26 09:01:56 +0530

    update get all products api url

--- flutter-app/lib/services/product.dart
+++ flutter-app/lib/services/product.dart
@@ -8,7 +8,7 @@ Future<dynamic> getAllProducts(
     {int? limit, bool? hasNext, String? nextId}) async {
   limit = limit != null ? limit : 10;
 
-  String baseURL = '${dotenv.env["BACKEND_API_BASE_URL"]}produc?limit=$limit';
+  String baseURL = '${dotenv.env["BACKEND_API_BASE_URL"]}product?limit=$limit';
   if (hasNext == true) {
     baseURL = '$baseURL&next=$nextId';
   }
@@ -25,6 +25,7 @@ Future<dynamic> getAllProducts(
   if (response[0] != null) {
     Response<dynamic> res = response[0] as Response<dynamic>;
     response[0] = res.data;
+    print(response);
 
     if (!response[0]['error']) {
       List<dynamic> products = response[0]['data']['products'];
