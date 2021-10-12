68fb9a0a552eabee31fa898eb58648f5e0c8c605
AkashSDas <aakashdas368@gmail.com>
2021-10-12 18:17:31 +0530

    create success and failed snackbar

--- flutter-app/lib/widgets/common/snackbar.dart
+++ flutter-app/lib/widgets/common/snackbar.dart
@@ -14,3 +14,37 @@ ScaffoldFeatureController<SnackBar, SnackBarClosedReason> displaySnackBar({
     backgroundColor: success ? Colors.green : Colors.red,
   ));
 }
+
+ScaffoldFeatureController<SnackBar, SnackBarClosedReason> successSnackBar({
+  required BuildContext context,
+  required String msg,
+}) {
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        msg,
+        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+              color: Colors.white,
+            ),
+      ),
+      backgroundColor: Colors.green,
+    ),
+  );
+}
+
+ScaffoldFeatureController<SnackBar, SnackBarClosedReason> failedSnackBar({
+  required BuildContext context,
+  required String msg,
+}) {
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        msg,
+        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+              color: Colors.white,
+            ),
+      ),
+      backgroundColor: Colors.red,
+    ),
+  );
+}
