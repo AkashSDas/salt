19bdb75f0c79dc1f16bcdc3be1bbd76b5c4b90a0
AkashSDas <aakashdas368@gmail.com>
2021-10-13 14:30:24 +0530

    create cart product item widget

--- flutter-app/lib/screens/cart.dart
+++ flutter-app/lib/screens/cart.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:salt/designs/designs.dart';
 import 'package:salt/services/product.dart';
+import 'package:salt/widgets/common/btns.dart';
+import 'package:salt/widgets/common/snackbar.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -27,7 +30,10 @@ class _CartScreenState extends State<CartScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        color: Theme.of(context).primaryColor,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+        ),
+        padding: EdgeInsets.all(16),
         child: FutureBuilder(
           future: _getAllProductsInCart,
           builder: (context, snapshot) {
@@ -37,14 +43,162 @@ class _CartScreenState extends State<CartScreen> {
             /// created by object that have no null values exists
             final _data = jsonDecode(snapshot.data.toString()) as List;
             return ListView.builder(
+              clipBehavior: Clip.none,
               itemCount: _data.length,
-              itemBuilder: (context, idx) => Container(
-                child: Column(
-                  children: [],
-                ),
+              itemBuilder: (context, idx) => CartProductCard(
+                product: _data[idx],
               ),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class CartProductCard extends StatefulWidget {
+  final product;
+  const CartProductCard({required this.product, Key? key}) : super(key: key);
+
+  @override
+  _CartProductCardState createState() => _CartProductCardState();
+}
+
+class _CartProductCardState extends State<CartProductCard> {
+  int quantity = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    quantity = widget.product['quantity'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: DesignSystem.subtleBoxShadow,
+      ),
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildCoverImg(),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.product['title'],
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  SizedBox(height: 8),
+                  Text('Price: ${widget.product["price"]}'),
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: DesignSystem.grey4,
+                              width: 0.5,
+                            )),
+                        child: IconButton(
+                          onPressed: () {
+                            /// TODO: add constraint on how much can be added
+                            setState(() {
+                              quantity = quantity + 1;
+                            });
+                          },
+                          icon: Icon(Icons.add),
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        // decoration: BoxDecoration(
+                        //     borderRadius: BorderRadius.circular(12),
+                        //     border: Border.all(
+                        //       color: DesignSystem.grey4,
+                        //       width: 0.5,
+                        //     )),
+                        child: Center(
+                          child: Text(
+                            '$quantity',
+                            style:
+                                Theme.of(context).textTheme.bodyText1?.copyWith(
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: DesignSystem.grey4,
+                              width: 0.5,
+                            )),
+                        child: IconButton(
+                          onPressed: () {
+                            /// TODO: add constraint on how much can be substracted
+                            setState(() {
+                              quantity = quantity - 1;
+                            });
+                          },
+                          icon: Icon(Icons.remove),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    width: double.infinity,
+                    child: ExpandedButton(
+                      text: 'Remove',
+                      onPressed: () async {
+                        /// TODO: Check for auth before adding things to cart
+                        String productId = widget.product['id'];
+                        var response = await removeProductFromCart(productId);
+                        if (response)
+                          successSnackBar(
+                            context: context,
+                            msg: 'Successfully removed item from cart',
+                          );
+                        else
+                          failedSnackBar(
+                            context: context,
+                            msg: 'Failed to removed item from cart',
+                          );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCoverImg() {
+    return Container(
+      height: 200,
+      width: 150,
+      margin: EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+        color: DesignSystem.grey1,
+        borderRadius: BorderRadius.circular(12),
+        image: DecorationImage(
+          image: NetworkImage(widget.product['coverImgURLs'][0]),
+          fit: BoxFit.cover,
         ),
       ),
     );
