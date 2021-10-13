486576614c6aef1caa5ff9cb0b82026c68f11581
AkashSDas <aakashdas368@gmail.com>
2021-10-13 10:09:09 +0530

    refactor signup tab in auth screen

--- flutter-app/lib/widgets/auth-screen/signup.dart
+++ flutter-app/lib/widgets/auth-screen/signup.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:salt/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:salt/designs/designs.dart';
+import 'package:salt/providers/signup.dart';
 import 'package:salt/utils/form/validators.dart';
 import 'package:salt/widgets/common/btns.dart';
 import 'package:salt/widgets/common/snackbar.dart';
-import 'package:salt/widgets/form/input.dart';
 
 class Signup extends StatefulWidget {
   const Signup({Key? key}) : super(key: key);
@@ -16,41 +17,6 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   final _formKey = GlobalKey<FormState>();
   final _validator = SignUpFormValidators();
-  Map<String, String> _formData = {'email': '', 'username': '', 'password': ''};
-
-  /// Submit
-  void _submit() async {
-    bool checkUsername = _validator.username.isValid(_formData['username']);
-    bool checkEmail = _validator.email.isValid(_formData['email']);
-    bool checkPassword = _validator.password.isValid(_formData['password']);
-
-    if (!checkUsername)
-      failedSnackBar(context: context, msg: 'Invalid username');
-    else if (!checkEmail)
-      failedSnackBar(context: context, msg: 'Invalid email');
-    else if (!checkPassword)
-      failedSnackBar(context: context, msg: 'Invalid password');
-    else {
-      final response = await signup(_formData);
-
-      if (response[1] != null)
-
-        /// Any error in calling
-        failedSnackBar(
-          context: context,
-          msg: 'Something went wrong, Please try again',
-        );
-      else {
-        if (response[0]['error'])
-          failedSnackBar(context: context, msg: response[0]['message']);
-        else {
-          successSnackBar(context: context, msg: response[0]['message']);
-          await Future.delayed(Duration(seconds: 3));
-          Navigator.popAndPushNamed(context, '/');
-        }
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,52 +26,151 @@ class _SignupState extends State<Signup> {
         key: _formKey,
         child: ListView(
           children: [
-            _buildUsernameInputField(),
+            _buildUsernameInputField(context),
             _buildSpace(),
-            _buildEmailInputField(),
+            _buildEmailInputField(context),
             _buildSpace(),
-            _buildPasswordInputField(),
+            _buildPasswordInputField(context),
             _buildSpace(),
-            _buildSignUpBtn(),
+            _buildSignUpBtn(context),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildSignUpBtn() {
+  Widget _buildSignUpBtn(BuildContext context) {
+    SignUpFormProvider _provider = Provider.of<SignUpFormProvider>(context);
     return ExpandedButton(
-      text: 'Sign up',
+      text: _provider.loading ? 'Loading...' : 'Sign up',
       verticalPadding: 20,
-      onPressed: () => _submit(),
+      onPressed: () async {
+        bool checkUsername = _validator.username.isValid(_provider.username);
+        bool checkEmail = _validator.email.isValid(_provider.email);
+        bool checkPassword = _validator.password.isValid(_provider.password);
+
+        if (!checkUsername)
+          failedSnackBar(context: context, msg: 'Invalid username');
+        else if (!checkEmail)
+          failedSnackBar(context: context, msg: 'Invalid email');
+        else if (!checkPassword)
+          failedSnackBar(context: context, msg: 'Invalid password');
+        else {
+          _provider.userSignUp(context);
+        }
+      },
     );
   }
 
   Widget _buildSpace() => SizedBox(height: 32);
 
-  Widget _buildUsernameInputField() => FormInputField(
-        'username',
-        'John Smith',
-        'Username',
-        _validator.username,
-        _formData,
-      );
+  Widget _buildUsernameInputField(BuildContext context) {
+    SignUpFormProvider _provider = Provider.of<SignUpFormProvider>(context);
+    return KFormInputField(
+      label: 'Username',
+      onChanged: (value) => _provider.updateFormValue('username', value),
+      hintText: 'John Simth',
+      validator: _validator.username,
+    );
+  }
 
-  Widget _buildPasswordInputField() => FormInputField(
-        'password',
-        'secure password',
-        'Password',
-        _validator.password,
-        _formData,
-        obscureText: true,
-      );
+  Widget _buildEmailInputField(BuildContext context) {
+    SignUpFormProvider _provider = Provider.of<SignUpFormProvider>(context);
+    return KFormInputField(
+      label: 'Email',
+      onChanged: (value) => _provider.updateFormValue('email', value),
+      hintText: 'john@gmail.com',
+      validator: _validator.email,
+      keyboardType: TextInputType.emailAddress,
+    );
+  }
 
-  Widget _buildEmailInputField() => FormInputField(
-        'email',
-        'john@gmail.com',
-        'Email',
-        _validator.email,
-        _formData,
-        keyboardType: TextInputType.emailAddress,
-      );
+  Widget _buildPasswordInputField(BuildContext context) {
+    SignUpFormProvider _provider = Provider.of<SignUpFormProvider>(context);
+    return KFormInputField(
+      label: 'Password',
+      onChanged: (value) => _provider.updateFormValue('password', value),
+      hintText: 'Secure password',
+      validator: _validator.password,
+      obscureText: true,
+    );
+  }
+}
+
+class KFormInputField extends StatelessWidget {
+  final String label;
+  final void Function(String) onChanged;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+  final String hintText;
+
+  const KFormInputField({
+    required this.label,
+    required this.onChanged,
+    required this.hintText,
+    this.validator,
+    this.keyboardType,
+    this.obscureText = false,
+    Key? key,
+  }) : super(key: key);
+
+  /// WIDGETS ///
+
+  Widget _buildLabel(BuildContext context) {
+    var style = Theme.of(context).textTheme.subtitle2?.copyWith(
+          fontWeight: FontWeight.w700,
+          fontSize: 15,
+        );
+
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(label, style: style),
+    );
+  }
+
+  InputDecoration _inputDecoration(BuildContext context) {
+    var border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(
+        width: 0,
+        style: BorderStyle.none,
+      ),
+    );
+
+    var hintStyle = Theme.of(context).textTheme.bodyText2?.copyWith(
+          color: DesignSystem.grey3.withOpacity(0.5),
+        );
+
+    return InputDecoration(
+      fillColor: DesignSystem.grey1,
+      filled: true,
+      floatingLabelBehavior: FloatingLabelBehavior.always,
+      hintText: hintText,
+      border: border,
+      hintStyle: hintStyle,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildLabel(context),
+        SizedBox(height: 4),
+        Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+          child: TextFormField(
+            textInputAction: TextInputAction.next,
+            obscureText: obscureText,
+            keyboardType: keyboardType,
+            onChanged: (value) => onChanged(value),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: validator,
+            decoration: _inputDecoration(context),
+          ),
+        ),
+      ],
+    );
+  }
 }
