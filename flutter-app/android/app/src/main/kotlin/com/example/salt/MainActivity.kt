5278a34cc6f3457ded0adf2ecde174ec64d819c3
AkashSDas <aakashdas368@gmail.com>
2021-10-15 08:04:48 +0530

    make config for flutter_stripe

--- flutter-app/android/app/src/main/kotlin/com/example/salt/MainActivity.kt
+++ flutter-app/android/app/src/main/kotlin/com/example/salt/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.salt
 
-import io.flutter.embedding.android.FlutterActivity
+// import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+// class MainActivity: FlutterActivity() {
+// }
+
+class MainActivity : FlutterFragmentActivity() {
 }
