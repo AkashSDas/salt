e9fe4cb8797fdd8c4e845601bd79187c387f5510
AkashSDas <aakashdas368@gmail.com>
2021-10-17 09:11:53 +0530

    refactor blog post editor label widget

--- flutter-app/lib/widgets/blog-post-editor/label.dart
+++ flutter-app/lib/widgets/blog-post-editor/label.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
-class FormInputLabel extends StatelessWidget {
+/// Label
+class BlogPostFormLabel extends StatelessWidget {
   final String label;
-  const FormInputLabel({required this.label, Key? key}) : super(key: key);
+  const BlogPostFormLabel({required this.label, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var _style = Theme.of(context).textTheme.subtitle2?.copyWith(
+          fontWeight: FontWeight.w900,
+          fontSize: 15,
+          color: Colors.black,
+        );
+
     return Container(
       alignment: Alignment.centerLeft,
-      child: Text(
-        label,
-        style: Theme.of(context).textTheme.subtitle2?.copyWith(
-              fontWeight: FontWeight.w900,
-              fontSize: 15,
-              color: Colors.black,
-            ),
-      ),
+      child: Text(label, style: _style),
     );
   }
 }
