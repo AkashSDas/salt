7b7a07ad903b949e44e6238dbb734254d214ed7b
AkashSDas <aakashdas368@gmail.com>
2021-10-22 19:25:18 +0530

    create design system in design.dart

---
+++ salt/lib/design.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DesignSystem {
+  /// Colors
+  static const white = Colors.white;
+  static const dodgerBlue = Color(0xFF1FA1FF);
+  static const alabaster = Color(0xFFF7F7F7);
+  static const tundora = Color(0xFF4B4B4B);
+  static const mineShaft = Color(0xFF373737);
+  static const ebonyClay = Color(0xFF2D3346);
+  static const boulder = Color(0xFF7D7D7D);
+  static const radicalRed = Color(0xFFFF3F56);
+  static const greenYellow = Color(0xFFB6FF3F);
+
+  /// Font Family
+  static const fontHead = 'Sofia Pro';
+  static const fontBody = 'Sofia Pro';
+
+  /// Font Syles
+
+}
