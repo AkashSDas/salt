e466a71def4a81f90caabea4a4e6dc0a935af375
AkashSDas <aakashdas368@gmail.com>
2021-10-23 08:29:50 +0530

    create auth_check.dart

---
+++ salt/lib/widgets/auth/auth_check.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:salt/providers/user.dart';
+
+class AuthCheck extends StatelessWidget {
+  final Widget child;
+  final bool displayOnAuth; // on authenticated
+
+  const AuthCheck({
+    required this.child,
+    this.displayOnAuth = true,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    UserProvider _user = Provider.of<UserProvider>(context);
+
+    /// display when user is authenticated
+    if (displayOnAuth && _user.user != null) return child;
+
+    /// display when user is not authenticated
+    if (!displayOnAuth && _user.user == null) return child;
+
+    return const SizedBox();
+  }
+}
