112493d127fb0737ea577db1768451c6df1d62e2
AkashSDas <aakashdas368@gmail.com>
2021-10-23 11:00:30 +0530

    create food category model

---
+++ salt/lib/models/food_category/food_category.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'food_category.g.dart';
+
+@JsonSerializable()
+class FoodCategory {
+  final String id;
+  final String emoji;
+  final String name;
+  final String description;
+
+  FoodCategory({
+    required this.id,
+    required this.emoji,
+    required this.name,
+    required this.description,
+  });
+
+  factory FoodCategory.fromJson(Map<String, dynamic> json) {
+    return _$FoodCategoryFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$FoodCategoryToJson(this);
+}
---
+++ salt/lib/models/food_category/food_category.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'food_category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FoodCategory _$FoodCategoryFromJson(Map<String, dynamic> json) {
+  return FoodCategory(
+    id: json['_id'] as String,
+    emoji: json['emoji'] as String,
+    name: json['name'] as String,
+    description: json['description'] as String,
+  );
+}
+
+Map<String, dynamic> _$FoodCategoryToJson(FoodCategory instance) =>
+    <String, dynamic>{
+      'emoji': instance.emoji,
+      'name': instance.name,
+      'description': instance.description,
+    };
