893dcea0a97d9d9ab68edf3d2fd7cf19aaceca41
AkashSDas <aakashdas368@gmail.com>
2021-10-23 10:33:53 +0530

    create rounded corner button widget

---
+++ salt/lib/widgets/buttons/index.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:salt/design_system.dart';
+
+class RoundedCornerButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final String text;
+  final double verticalPadding;
+
+  const RoundedCornerButton({
+    required this.onPressed,
+    required this.text,
+    this.verticalPadding = 16,
+    Key? key,
+  }) : super(key: key);
+
+  /// btn style
+  ButtonStyle _style(BuildContext context) {
+    return ButtonStyle(
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      ),
+      backgroundColor: MaterialStateProperty.all(
+        Theme.of(context).colorScheme.secondary,
+      ),
+      padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+        vertical: verticalPadding,
+      )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: _style(context),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontFamily: DesignSystem.fontHead,
+          fontWeight: FontWeight.w900,
+          letterSpacing: 2,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+}
