f310757053778fc14488e0b4e15f1e9b71717718
AkashSDas <aakashdas368@gmail.com>
2021-10-24 08:25:27 +0530

    create blog post model

---
+++ salt/lib/models/blog_post/blog_post.dart
@@ -0,0 +1,38 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:salt/models/food_category/food_category.dart';
+import 'package:salt/models/user/user.dart';
+
+part 'blog_post.g.dart';
+
+@JsonSerializable()
+class BlogPost {
+  final String id;
+  final String title;
+  final String description;
+  final String content;
+  final String coverImgURL;
+  final double readTime;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final List<FoodCategory> categories;
+  final User author;
+
+  BlogPost({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.content,
+    required this.coverImgURL,
+    required this.readTime,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.categories,
+    required this.author,
+  });
+
+  factory BlogPost.fromJson(Map<String, dynamic> json) {
+    return _$BlogPostFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$BlogPostToJson(this);
+}
