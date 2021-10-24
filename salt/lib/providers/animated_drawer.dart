c41df43bd120322161f66171bae21721808d9d9e
AkashSDas <aakashdas368@gmail.com>
2021-10-24 17:00:58 +0530

    add animate appbar along with appbarHeight and appbarBoxShadowOpacity properties

--- salt/lib/providers/animated_drawer.dart
+++ salt/lib/providers/animated_drawer.dart
@@ -11,4 +11,31 @@ class AnimatedDrawerProvider extends ChangeNotifier {
     isDrawerOpen = !isDrawerOpen;
     notifyListeners();
   }
+
+  double appbarHeight = 80 + 32;
+  double appbarBoxShadowOpacity = 0;
+
+  void animateAppBar(double scrollPixelsValue) {
+    /// Some math for height
+    /// 0 - 96 scroll px
+    /// 80 + 32 = 112 -- 64 (height of appbar when we've scrolled 96px)
+    ///
+    /// 112 - 64 == 49
+    /// 49 / 96 == 0.666 == 0.51
+    ///
+    /// For opactiy
+    /// 0 - 0.25
+    /// 0.25 / 96 == 0.0026
+
+    if (scrollPixelsValue <= 96) {
+      appbarHeight = 112 - (scrollPixelsValue * 0.51);
+
+      if (scrollPixelsValue == 0) {
+        appbarBoxShadowOpacity = 0;
+      } else {
+        appbarBoxShadowOpacity = 0 + (scrollPixelsValue * 0.002604166666667);
+      }
+      notifyListeners();
+    }
+  }
 }
