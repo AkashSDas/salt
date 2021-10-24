053c782845c782cbe0ea69cf32d6130d34e233c3
AkashSDas <aakashdas368@gmail.com>
2021-10-24 12:23:42 +0530

    create infinite scroll blog post section in home screen

--- salt/lib/main.dart
+++ salt/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:salt/design_system.dart';
+import 'package:salt/providers/blog_post_infinite_scroll.dart';
 import 'package:salt/providers/user.dart';
 import 'package:salt/widgets/animated_drawer/animated_drawer.dart';
+import 'package:salt/widgets/blog_post/blog_post_listview.dart';
+import 'package:salt/widgets/blog_post/blog_post_listview_utils.dart';
 import 'package:salt/widgets/food_category/inline_food_category.dart';
+import 'package:salt/widgets/headers/index.dart';
 import 'package:salt/widgets/recipe/inline_recipes.dart';
 
 void main() async {
@@ -26,15 +30,66 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: DesignSystem.theme,
         routes: {
-          '/': (context) => HomeScreen(),
+          '/': (context) => const BlogPostInfiniteScrollWrapper(
+                child: HomeScreen(),
+              ),
         },
       ),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final ScrollController _ctrl = ScrollController();
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      Provider.of<BlogPostInfiniteScrollProvider>(
+        context,
+        listen: false,
+      ).initialFetch();
+
+      /// Scroll event for fetching more posts
+      _ctrl.addListener(() {
+        var loading = Provider.of<BlogPostInfiniteScrollProvider>(
+          context,
+          listen: false,
+        ).loading;
+
+        var reachedEnd = Provider.of<BlogPostInfiniteScrollProvider>(
+          context,
+          listen: false,
+        ).reachedEnd;
+
+        var pixels = _ctrl.position.pixels;
+        var maxScrollExtent = _ctrl.position.maxScrollExtent;
+
+        if (pixels >= maxScrollExtent && !loading && !reachedEnd) {
+          /// fetch more
+          Provider.of<BlogPostInfiniteScrollProvider>(
+            context,
+            listen: false,
+          ).fetchMore();
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,10 +97,16 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8).copyWith(top: 32),
         child: ListView(
+          controller: _ctrl,
           children: [
             InlineFoodCategory(),
             const SizedBox(height: 32),
             InlineRecipes(),
+            const SizedBox(height: 32),
+            const Heading(title: 'Most Popular Posts'),
+            const SizedBox(height: 16),
+            const BlogPostListView(),
+            const BlogPostListViewEnd(),
           ],
         ),
       ),
