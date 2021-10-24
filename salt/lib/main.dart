f1db0439b8310361c47413c4708819efe8eec5bb
AkashSDas <aakashdas368@gmail.com>
2021-10-24 13:45:00 +0530

    extracgt home screen widget to separate file

--- salt/lib/main.dart
+++ salt/lib/main.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:salt/design_system.dart';
-import 'package:salt/providers/blog_post_infinite_scroll.dart';
 import 'package:salt/providers/user.dart';
-import 'package:salt/widgets/animated_drawer/animated_drawer.dart';
-import 'package:salt/widgets/blog_post/blog_post_listview.dart';
+import 'package:salt/screens/home.dart';
 import 'package:salt/widgets/blog_post/blog_post_listview_utils.dart';
-import 'package:salt/widgets/food_category/inline_food_category.dart';
-import 'package:salt/widgets/headers/index.dart';
-import 'package:salt/widgets/recipe/inline_recipes.dart';
 
 void main() async {
   /// Loading env variables
@@ -34,83 +29,6 @@ class MyApp extends StatelessWidget {
                 child: HomeScreen(),
               ),
         },
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final ScrollController _ctrl = ScrollController();
-
-  @override
-  void dispose() {
-    _ctrl.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
-      Provider.of<BlogPostInfiniteScrollProvider>(
-        context,
-        listen: false,
-      ).initialFetch();
-
-      /// Scroll event for fetching more posts
-      _ctrl.addListener(() {
-        var loading = Provider.of<BlogPostInfiniteScrollProvider>(
-          context,
-          listen: false,
-        ).loading;
-
-        var reachedEnd = Provider.of<BlogPostInfiniteScrollProvider>(
-          context,
-          listen: false,
-        ).reachedEnd;
-
-        var pixels = _ctrl.position.pixels;
-        var maxScrollExtent = _ctrl.position.maxScrollExtent;
-
-        if (pixels >= maxScrollExtent && !loading && !reachedEnd) {
-          /// fetch more
-          Provider.of<BlogPostInfiniteScrollProvider>(
-            context,
-            listen: false,
-          ).fetchMore();
-        }
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedDrawer(
-      body: Padding(
-        padding: const EdgeInsets.all(8).copyWith(top: 0),
-        child: ListView(
-          controller: _ctrl,
-          clipBehavior: Clip.none,
-          addAutomaticKeepAlives: true,
-          children: const [
-            InlineCategory(),
-            SizedBox(height: 32),
-            InlineRecipes(),
-            SizedBox(height: 32),
-            Heading(title: 'Most Popular Posts'),
-            SizedBox(height: 16),
-            BlogPostListView(),
-            BlogPostListViewEnd(),
-          ],
-        ),
       ),
     );
   }
