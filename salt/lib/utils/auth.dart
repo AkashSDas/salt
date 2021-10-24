8faee4add6bbe4e0994eb87f20e889460b89a7e1
AkashSDas <aakashdas368@gmail.com>
2021-10-24 22:56:08 +0530

    create auth validators

---
+++ salt/lib/utils/auth.dart
@@ -0,0 +1,39 @@
+import 'package:form_field_validator/form_field_validator.dart';
+
+class LoginFormValidators {
+  final email = MultiValidator([
+    RequiredValidator(errorText: 'Email is required'),
+    EmailValidator(errorText: 'Enter a valid email address'),
+  ]);
+
+  final password = MultiValidator([
+    RequiredValidator(errorText: 'Password is required'),
+    MinLengthValidator(
+      6,
+      errorText: 'Password should be atleast of 6 characters',
+    ),
+  ]);
+}
+
+class SignUpFormValidators {
+  final username = MultiValidator([
+    RequiredValidator(errorText: 'Username is required'),
+    MinLengthValidator(
+      3,
+      errorText: 'Username must be at least 3 characters long',
+    ),
+  ]);
+
+  final email = MultiValidator([
+    RequiredValidator(errorText: 'Email is required'),
+    EmailValidator(errorText: 'Enter a valid email address'),
+  ]);
+
+  final password = MultiValidator([
+    RequiredValidator(errorText: 'Password is required'),
+    MinLengthValidator(
+      6,
+      errorText: 'Password should be atleast of 6 characters',
+    ),
+  ]);
+}
