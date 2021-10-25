c9358cf7050cc253e6bff6dd826d9a4903d1be58
AkashSDas <aakashdas368@gmail.com>
2021-10-25 15:37:33 +0530

    create rounded corner icon button widget

--- salt/lib/widgets/buttons/index.dart
+++ salt/lib/widgets/buttons/index.dart
@@ -49,3 +49,53 @@ class RoundedCornerButton extends StatelessWidget {
     );
   }
 }
+
+class RoundedCornerIconButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final String text;
+  final double verticalPadding;
+  final double horizontalPadding;
+  final Widget icon;
+
+  const RoundedCornerIconButton({
+    required this.onPressed,
+    required this.text,
+    required this.icon,
+    this.verticalPadding = 14,
+    this.horizontalPadding = 0,
+    Key? key,
+  }) : super(key: key);
+
+  /// btn style
+  ButtonStyle _style(BuildContext context) {
+    return ButtonStyle(
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      ),
+      backgroundColor: MaterialStateProperty.all(DesignSystem.gallery),
+      padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+        vertical: verticalPadding,
+        horizontal: horizontalPadding,
+      )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton.icon(
+      style: _style(context),
+      onPressed: onPressed,
+      icon: icon,
+      label: Text(
+        text,
+        style: const TextStyle(
+          color: DesignSystem.tundora,
+          fontFamily: DesignSystem.fontHead,
+          fontWeight: FontWeight.w500,
+          fontSize: 15,
+          height: 1.4,
+        ),
+      ),
+    );
+  }
+}
