87932945609ff71ab0a4d59d345d4dd65f517ef9
AkashSDas <aakashdas368@gmail.com>
2021-10-28 08:34:21 +0530

    create skeleton of products listview

---
+++ salt/lib/widgets/product/products_listview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsListView extends StatelessWidget {
+  const ProductsListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
