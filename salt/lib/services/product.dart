e55e4b7c885dcd81931abe64b0b07c6f148159e9
AkashSDas <aakashdas368@gmail.com>
2021-10-28 10:17:16 +0530

    create save to cart service

--- salt/lib/services/product.dart
+++ salt/lib/services/product.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:salt/models/product/product.dart';
 import 'package:salt/utils/index.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as Storage;
 
 class ProductService {
   late bool error;
@@ -59,5 +62,66 @@ class ProductService {
       products.add(Product.fromJson(data['products'][i]));
     }
     return products;
+  }
+
+  Future<void> saveProductToCart(Map product) async {
+    /// Shape of `cart products` will be
+    /// {
+    ///   'cart-products': [
+    ///       product1,
+    ///       product2,
+    ///       ...
+    ///   ]
+    /// }
+
+    var _storage = const Storage.FlutterSecureStorage();
+
+    /// Check if cart exists
+    final cartResponse = await runAsync(_storage.read(key: 'cart'));
+    if (cartResponse[1] != null) {
+      /// error
+      error = true;
+      msg = 'Something went wrong, Please try again';
+      return;
+    }
+
+    if (cartResponse[0] == null) {
+      /// Cart doesn't exists
+
+      final createResponse = await runAsync(
+        _storage.write(key: 'cart', value: jsonEncode([product])),
+      );
+
+      if (createResponse[1] != null) {
+        /// error
+        error = true;
+        msg = 'Something went wrong, Please try again';
+        return;
+      }
+      msg = 'Successfully added to cart';
+      return;
+    }
+
+    /// Cart exists
+    /// Deserialize cart, cart here will be list of products
+    List cart = jsonDecode(cartResponse[0]);
+
+    /// Removing the product if the cart already had it
+    cart.where((prod) => prod['id'] != product['id']);
+
+    /// Then adding the item to the cart
+    cart.add(product);
+    final addResponse = await runAsync(
+      _storage.write(key: 'cart', value: jsonEncode(cart)),
+    );
+
+    if (addResponse[1] != null) {
+      /// error
+      error = true;
+      msg = 'Something went wrong, Please try again';
+      return;
+    }
+
+    msg = 'Successfully added to cart';
   }
 }
