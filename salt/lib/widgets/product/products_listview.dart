7d530ad3a0921cc27547c540f8a57451134af862
AkashSDas <aakashdas368@gmail.com>
2021-10-28 10:17:51 +0530

    add add to cart btn for product in products listview

--- salt/lib/widgets/product/products_listview.dart
+++ salt/lib/widgets/product/products_listview.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:salt/design_system.dart';
+import 'package:salt/providers/cart.dart';
 import 'package:salt/providers/products_infinite_scroll.dart';
+import 'package:salt/widgets/buttons/index.dart';
 
 class ProductsListView extends StatelessWidget {
   const ProductsListView({Key? key}) : super(key: key);
@@ -38,45 +40,63 @@ class _ProductsGridView extends StatelessWidget {
         crossAxisCount: 2,
         mainAxisSpacing: 8,
         crossAxisSpacing: 8,
-        childAspectRatio: 0.7,
+        childAspectRatio: 0.575,
       ),
       itemBuilder: (context, idx) {
         var product = _provider.products[idx];
 
-        return Container(
-          decoration: BoxDecoration(
-            color: DesignSystem.gallery,
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _CoverImage(
-                coverImgURL: product.coverImgURLs[0],
-              ),
-              const SizedBox(height: 12),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 6),
-                child: Text(
-                  '\$${product.price}',
-                  style: DesignSystem.bodyMain.copyWith(
-                    fontWeight: FontWeight.w700,
-                  ),
+        return ChangeNotifierProvider(
+          create: (context) => CartProvider(),
+          child: Container(
+            decoration: BoxDecoration(
+              color: DesignSystem.gallery,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _CoverImage(
+                  coverImgURL: product.coverImgURLs[0],
                 ),
-              ),
-              const SizedBox(height: 12),
-              Padding(
-                padding: const EdgeInsets.only(left: 6, right: 6, bottom: 6),
-                child: Flexible(
+                const SizedBox(height: 12),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 6),
                   child: Text(
-                    product.title,
-                    style: DesignSystem.caption,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+                    '\$${product.price}',
+                    style: DesignSystem.bodyMain.copyWith(
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 12),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6, right: 6, bottom: 6),
+                  child: Flexible(
+                    child: Text(
+                      product.title,
+                      style: DesignSystem.caption,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Builder(builder: (ctx) {
+                  final _p = Provider.of<CartProvider>(ctx);
+                  return SizedBox(
+                    width: double.infinity,
+                    child: RoundedCornerButton(
+                      onPressed: () async {
+                        var prod = product.toJson();
+                        prod = {...prod, 'quantity': 1};
+                        await _p.saveProductToCart(context, prod);
+                      },
+                      text: _p.loading ? 'Saving...' : 'Add to cart',
+                    ),
+                  );
+                }),
+              ],
+            ),
           ),
         );
       },
