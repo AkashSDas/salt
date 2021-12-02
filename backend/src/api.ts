99e2d8ebc45dfb9f3b744be8531533f0cddec1c7
AkashSDas <aakashdas368@gmail.com>
2021-12-02 19:52:04 +0530

    use auth router in app

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express from "express";
+import { router as authRouter } from "./routes/auth";
 
 // App
 export const app = express();
@@ -9,4 +10,7 @@ app.use(cors());
 app.use(express.json()); // for parsing incoming data
 
 // Test api routes
-app.get("/", (_, res) => res.send("hello world"));
+// app.get("/", (_, res) => res.send("hello world"));
+
+// Routes
+app.use("/api/auth", authRouter);
