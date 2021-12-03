9f9ec056b88367cde277f6c3169e3abf0b22c921
AkashSDas <aakashdas368@gmail.com>
2021-12-03 09:36:46 +0530

    create routes for tag

---
+++ backend/src/routes/tags.ts
@@ -0,0 +1,46 @@
+/**
+ * Base route for tags is `/tags`
+ */
+
+import { Router } from "express";
+import { createTag, deleteTag, getAllTags } from "../controllers/tag";
+import { isAuthenticated, isLoggedIn } from "../middlewares/auth";
+import { validationCheck } from "../middlewares/express_validation";
+import { getTagById } from "../middlewares/tag";
+import { getUserById, isAdmin } from "../middlewares/user";
+import { tagValidation } from "../validators";
+
+export const router = Router();
+
+/**
+ * Params
+ */
+router.param("userId", getUserById);
+router.param("tagId", getTagById);
+
+/**
+ * Routes
+ */
+
+// Get all tags
+router.get("/", getAllTags);
+
+// Create tag when requested by an admin
+router.post(
+  "/:userId",
+  isLoggedIn,
+  isAuthenticated,
+  isAdmin,
+  tagValidation,
+  validationCheck,
+  createTag
+);
+
+// Delete tag when requested by an admin
+router.delete(
+  "/:userId/:tagId",
+  isLoggedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteTag
+);
