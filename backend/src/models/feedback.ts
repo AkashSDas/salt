bf031c9bad438967998f7f4208a0ffc5cb3ffa05
AkashSDas <aakashdas368@gmail.com>
2021-12-03 18:16:36 +0530

    create a feedback model

---
+++ backend/src/models/feedback.ts
@@ -0,0 +1,27 @@
+/**
+ * Model purpose
+ *
+ * Users who have purchased a product can rate the it and give their comment
+ * on that product.
+ */
+
+import { Document, model, Schema } from "mongoose";
+import { ProductDocument } from "./product";
+import { UserDocument } from "./user";
+
+export type FeedbackDocument = Document & {
+  userId: UserDocument;
+  productId: ProductDocument;
+  rating: number; // ranging from 0-5 (discret number)
+  comment: string;
+};
+
+const FeedbackSchema = new Schema<FeedbackDocument>({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  rating: { type: Number, required: true, min: 0, max: 5 },
+  comment: { type: String, required: true, trim: true },
+});
+
+const Feedback = model<FeedbackDocument>("Feedback", FeedbackSchema);
+export default Feedback;
