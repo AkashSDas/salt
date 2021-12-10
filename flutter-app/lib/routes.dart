bfdd7793c320b4fef4b001d4d85639c6839b67af
AkashSDas <aakashdas368@gmail.com>
2021-12-10 19:22:57 +0530

    change index route to app splash screen

--- flutter-app/lib/routes.dart
+++ flutter-app/lib/routes.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:salt/screens/home.dart';
+import 'package:salt/screens/app_splash_screen.dart';
 
 Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
   return {
-    '/': (context) => const HomeScreen(),
+    '/': (context) => const AppSplashScreen(),
   };
 }
