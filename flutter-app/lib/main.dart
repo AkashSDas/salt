dd7c559b7c932cbb17b5fafdcf30d53cf51b8831
AkashSDas <aakashdas368@gmail.com>
2021-12-10 19:03:50 +0530

    add bg color to splash screen and update system ui overlay style

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:salt/design_system.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: DesignSystem.primary,
+      statusBarColor: DesignSystem.primary,
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,6 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'salt',
       debugShowCheckedModeBanner: false,
+      theme: DesignSystem.theme,
       routes: {
         '/': (context) => SafeArea(
               child: SplashScreen.navigate(
@@ -18,6 +30,7 @@ class MyApp extends StatelessWidget {
                 next: (context) => const HomeScreen(),
                 until: () => Future.delayed(const Duration(seconds: 4)),
                 startAnimation: 'go',
+                backgroundColor: DesignSystem.primary,
               ),
             )
       },
