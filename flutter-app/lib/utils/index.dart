5ebf850f2ee0b640ac28688bdca2925fdf462780
AkashSDas <aakashdas368@gmail.com>
2021-12-11 13:03:22 +0530

    create create random string util function

---
+++ flutter-app/lib/utils/index.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+/// Create random string
+///
+/// It returns a string of length [length] that's generated randomly
+String createRandomString(int length) {
+  var rnd = Random();
+  const chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+
+  return String.fromCharCodes(
+    Iterable.generate(
+      length,
+      (_) => chars.codeUnitAt(rnd.nextInt(chars.length)),
+    ),
+  );
+}
