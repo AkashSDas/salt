4619496676e241c3c476dc0700821b7a522c1bde
AkashSDas <aakashdas368@gmail.com>
2021-12-12 13:01:53 +0530

    refactor others recipe posts inlineview into smaller widgets

--- flutter-app/lib/widgets/post/others_recipe_posts_inlineview.dart
+++ flutter-app/lib/widgets/post/others_recipe_posts_inlineview.dart
@@ -19,62 +19,7 @@ class OthersRecipePostsInlineView extends StatelessWidget {
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           height: 200,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              ...(List.generate(
-                8,
-                (idx) => Container(
-                  height: 200,
-                  width: 130,
-                  margin: EdgeInsets.only(right: idx == 7 ? 0 : 16),
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).cardColor,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Stack(
-                    children: [
-                      /// Image
-                      Container(
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              'https://images.unsplash.com/photo-1639189972760-566d9ae1d4d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=463&q=80',
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-
-                      /// Mask
-                      Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [Color(0xff1F1F1F), Colors.transparent],
-                          ),
-                        ),
-                        child: Container(
-                          padding: const EdgeInsets.all(4),
-                          alignment: Alignment.bottomLeft,
-                          child: Text(
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus',
-                            style: DesignSystem.caption.copyWith(
-                              color: DesignSystem.text1,
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ).toList())
-            ],
-          ),
+          child: const _InlineView(),
         ),
         const SizedBox(height: 20),
         SecondaryButton(
@@ -83,6 +28,80 @@ class OthersRecipePostsInlineView extends StatelessWidget {
           horizontalPadding: 64,
         ),
       ],
+    );
+  }
+}
+
+class _InlineView extends StatelessWidget {
+  const _InlineView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        ...(List.generate(
+          8,
+          (idx) => Container(
+            height: 200,
+            width: 130,
+            margin: EdgeInsets.only(right: idx == 7 ? 0 : 16),
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Stack(children: const [_CoverImage(), _MaskWithTitle()]),
+          ),
+        ).toList())
+      ],
+    );
+  }
+}
+
+class _CoverImage extends StatelessWidget {
+  const _CoverImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            'https://images.unsplash.com/photo-1639189972760-566d9ae1d4d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=463&q=80',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _MaskWithTitle extends StatelessWidget {
+  const _MaskWithTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          colors: [Color(0xff1F1F1F), Colors.transparent],
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        alignment: Alignment.bottomLeft,
+        child: Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus',
+          style: DesignSystem.caption.copyWith(
+            color: DesignSystem.text1,
+          ),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
     );
   }
 }
