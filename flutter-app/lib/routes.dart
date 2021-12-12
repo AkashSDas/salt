96172471e4b4499d25e85d16176a1d1d0c629a07
AkashSDas <aakashdas368@gmail.com>
2021-12-12 21:02:07 +0530

    create route for singup screen

--- flutter-app/lib/routes.dart
+++ flutter-app/lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:salt/screens/app_splash_screen.dart';
+import 'package:salt/screens/signup.dart';
 
 Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
   return {
     '/': (context) => const AppSplashScreen(),
+    '/auth/signup': (context) => const SignupScreen(),
   };
 }
