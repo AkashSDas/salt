7d927fdb3092dbdf417e02b17d726e3fececd0d4
AkashSDas <aakashdas368@gmail.com>
2021-12-12 10:14:36 +0530

    extract preset tag and add groceries covers and heading in home screen

--- flutter-app/lib/screens/home.dart
+++ flutter-app/lib/screens/home.dart
@@ -1,6 +1,9 @@
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:salt/widgets/drawer/animated_drawer.dart';
+import 'package:salt/widgets/product/covers.dart';
+import 'package:salt/widgets/product/heading.dart';
+
+import '../widgets/drawer/animated_drawer.dart';
+import '../widgets/tag/preset_tag_section.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,92 +11,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedDrawer(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              PresetTagAnimatedButton(flareFilename: 'cake'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'chocolate'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'fast-food'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'sweet'),
-            ],
+      child: ListView(
+        children: const [
+          SizedBox(height: 0),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: PresetTagSection(),
           ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              PresetTagAnimatedButton(flareFilename: 'diet'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'non-veg'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'high-protein'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'movie-snack'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'lunch'),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              PresetTagAnimatedButton(flareFilename: 'sushi'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'dairy'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'sea-food'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'ice-cream'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'snack'),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              PresetTagAnimatedButton(flareFilename: 'fruit'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'caffeine'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'drink'),
-              SizedBox(width: 16),
-              PresetTagAnimatedButton(flareFilename: 'kitchen'),
-            ],
-          ),
+          SizedBox(height: 40),
+          GroceriesSectionHeading(),
+          SizedBox(height: 20),
+          GroceriesCovers(),
+          SizedBox(height: 20),
         ],
-      ),
-    );
-  }
-}
-
-class PresetTagAnimatedButton extends StatelessWidget {
-  final String flareFilename;
-
-  const PresetTagAnimatedButton({
-    required this.flareFilename,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 63,
-      width: 63,
-      padding: const EdgeInsets.all(13),
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.circular(32),
-      ),
-      child: FlareActor(
-        'assets/flare/tags-section/$flareFilename.flr',
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        animation: 'blink',
       ),
     );
   }
