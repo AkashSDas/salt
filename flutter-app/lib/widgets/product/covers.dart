d97cd261209558f694b6bbdbc998331358ef1532
AkashSDas <aakashdas368@gmail.com>
2021-12-12 14:38:37 +0530

    add error doc in groceries covers widget

--- flutter-app/lib/widgets/product/covers.dart
+++ flutter-app/lib/widgets/product/covers.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+/// There is a [RenderFlex] overflowed by 5.0 pixels on the right error
 class GroceriesCovers extends StatelessWidget {
   const GroceriesCovers({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Row(
         children: [
@@ -68,3 +69,29 @@ class GroceriesCoverImage extends StatelessWidget {
     );
   }
 }
+
+/// In [GroceriesCovers], instead of [Row] the below code grid view can be used.
+/// The only issue it faces is the image height (cover/contain) issue. This won't
+/// solve the [RenderFlex] overflowed error on [GroceriesCovers]
+//
+// StaggeredGridView.countBuilder(
+//         shrinkWrap: true,
+//         physics: const NeverScrollableScrollPhysics(),
+//         crossAxisCount: 2,
+//         itemCount: 3,
+//         itemBuilder: (context, idx) {
+//           double height = 138;
+//           if (idx == 1) height = 284;
+//           const imgs = ['dairy-products', 'fresh-veggies', 'fresh-fruities'];
+//           return GroceriesCoverImage(
+//             imgPath: imgs[idx],
+//             width: 178,
+//             height: height,
+//           );
+//         },
+//         staggeredTileBuilder: (idx) {
+//           return StaggeredTile.count(1, idx == 1 ? 2 : 1);
+//         },
+//         mainAxisSpacing: 8,
+//         crossAxisSpacing: 8,
+//       ),
