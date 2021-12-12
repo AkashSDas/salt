6f4c84a9cb539b8e9da7e0cf31de69d3a40839c9
AkashSDas <aakashdas368@gmail.com>
2021-12-12 10:50:06 +0530

    animate home screen appbar on scroll

--- flutter-app/lib/screens/home.dart
+++ flutter-app/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:salt/providers/animated_drawer.dart';
 import 'package:salt/widgets/product/covers.dart';
 import 'package:salt/widgets/product/heading.dart';
 import 'package:salt/widgets/product/preset_groceries.dart';
@@ -11,22 +13,63 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedDrawer(
-      child: ListView(
-        children: [
-          const SizedBox(height: 0),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: PresetTagSection(),
-          ),
-          const SizedBox(height: 40),
-          const GroceriesSectionHeading(),
-          const SizedBox(height: 20),
-          const GroceriesCovers(),
-          const SizedBox(height: 20),
-          const PresetGroceries(),
-        ],
-      ),
+    return const AnimatedDrawer(child: _HomeScreenListView());
+  }
+}
+
+/// Home Screen ListView
+
+class _HomeScreenListView extends StatefulWidget {
+  const _HomeScreenListView({Key? key}) : super(key: key);
+
+  @override
+  State<_HomeScreenListView> createState() => _HomeScreenListViewState();
+}
+
+class _HomeScreenListViewState extends State<_HomeScreenListView> {
+  final ScrollController _ctrl = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      _ctrl.addListener(() {
+        var pixels = _ctrl.position.pixels;
+        if (pixels >= 0) {
+          /// Listview has be scrolled (when == 0 you're at top)
+          Provider.of<AnimatedDrawerProvider>(
+            context,
+            listen: false,
+          ).animateAppBar(pixels);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      controller: _ctrl,
+      children: const [
+        SizedBox(height: 0),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+          child: PresetTagSection(),
+        ),
+        SizedBox(height: 40),
+        GroceriesSectionHeading(),
+        SizedBox(height: 20),
+        GroceriesCovers(),
+        SizedBox(height: 20),
+        PresetGroceries(),
+      ],
     );
   }
 }
