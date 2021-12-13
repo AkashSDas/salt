958e7a6c4772bf33ea682170358fe7dd21cc5316
AkashSDas <aakashdas368@gmail.com>
2021-12-13 14:06:08 +0530

    create login auth service

--- flutter-app/lib/services/auth.dart
+++ flutter-app/lib/services/auth.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:salt/utils/api.dart';
 import 'package:salt/utils/index.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as storage;
 
 class AuthService {
   static var baseURL = '${dotenv.env["BACKEND_API_BASE_URL"]}auth/';
@@ -26,5 +30,41 @@ class AuthService {
       msg: result['msg'],
       data: result['data'],
     );
+  }
+
+  /// Login
+  static Future<ApiResponse> login(Map<String, dynamic> data) async {
+    var res = await runAsync(
+      Dio().post('${baseURL}login', data: data, options: options),
+    );
+
+    if (res[0] == null) {
+      return ApiResponse(error: true, msg: ApiMessages.wentWrong, data: null);
+    }
+
+    Response apiRes = res[0] as Response;
+    var result = apiRes.data;
+    var apiResponse = ApiResponse(
+      error: result['error'],
+      msg: result['msg'],
+      data: result['data'],
+    );
+
+    if (!apiResponse.error) {
+      /// Save user data to secure storage
+      var _storage = const storage.FlutterSecureStorage();
+      var isSaved = await runAsync(
+        _storage.write(key: 'user', value: jsonEncode(apiResponse.data)),
+      );
+
+      /// Couldn't save user info
+      if (isSaved[1] != null) {
+        /// Making error true and response null
+        apiResponse.error = true;
+        apiResponse.msg = 'Something went wrong, Please try again';
+      }
+    }
+
+    return apiResponse;
   }
 }
