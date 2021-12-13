6abf3f86da019239320387e1a6111bd47c3973e4
AkashSDas <aakashdas368@gmail.com>
2021-12-13 10:59:33 +0530

    create runAsync util

--- flutter-app/lib/utils/index.dart
+++ flutter-app/lib/utils/index.dart
@@ -15,3 +15,13 @@ String createRandomString(int length) {
     ),
   );
 }
+
+/// To run async function without worring about try/catch blocks
+Future<List<dynamic>> runAsync(Future promise) async {
+  try {
+    final result = await promise;
+    return [result, null];
+  } catch (err) {
+    return [null, err];
+  }
+}
