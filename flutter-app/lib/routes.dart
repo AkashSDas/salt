6e8627f9a04a5a3d7f48e5a8d9bc527381e54bda
AkashSDas <aakashdas368@gmail.com>
2021-12-13 14:07:32 +0530

    add login route

--- flutter-app/lib/routes.dart
+++ flutter-app/lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:salt/screens/app_splash_screen.dart';
+import 'package:salt/screens/login.dart';
 import 'package:salt/screens/signup.dart';
 
 Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
   return {
     '/': (context) => const AppSplashScreen(),
     '/auth/signup': (context) => const SignupScreen(),
+    '/auth/login': (context) => const LoginScreen(),
   };
 }
