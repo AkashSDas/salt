dda19b34041de2d34b6cb7061dc56a489f3e513c
AkashSDas <aakashdas368@gmail.com>
2021-12-15 12:29:13 +0530

    setup flutter_stripe

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -4,12 +4,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:salt/providers/user_provider.dart';
 import 'package:salt/routes.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 import 'design_system.dart';
 
 void main() async {
   /// Loading env variables
   await dotenv.load(fileName: '.env');
+
+  /// Stripe
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = dotenv.env['STRIPE_KEY'].toString();
 
   runApp(const MyApp());
 
