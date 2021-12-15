b030ed8576711ef01636ce7dc4c7fa99a8183814
AkashSDas <aakashdas368@gmail.com>
2021-12-15 08:54:51 +0530

    add loading state in product provider

--- flutter-app/lib/providers/product.dart
+++ flutter-app/lib/providers/product.dart
@@ -4,12 +4,19 @@ class ProductProvider extends ChangeNotifier {
   double price; // price of 1 quantity
   int quantity = 1;
   double displayPrice;
+  var loading = false;
   ProductProvider({required this.price}) : displayPrice = price;
 
   /// Update product quantity
   void updateProductQuantity(int amount) {
     quantity = quantity + amount;
     displayPrice = price * quantity;
+    notifyListeners();
+  }
+
+  /// Update loading
+  void updateLoading(bool value) {
+    loading = value;
     notifyListeners();
   }
 }
