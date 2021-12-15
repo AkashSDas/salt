e2c171ce64bed40696a240f34e11be51c058d036
AkashSDas <aakashdas368@gmail.com>
2021-12-15 11:12:32 +0530

    create route for cart screen

--- flutter-app/lib/routes.dart
+++ flutter-app/lib/routes.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:salt/screens/app_splash_screen.dart';
 import 'package:salt/screens/login.dart';
 import 'package:salt/screens/signup.dart';
+import 'package:salt/screens/user_cart.dart';
 
 Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
   return {
     '/': (context) => const AppSplashScreen(),
     '/auth/signup': (context) => const SignupScreen(),
     '/auth/login': (context) => const LoginScreen(),
+    '/user/cart': (context) => const UserCartScreen(),
   };
 }
