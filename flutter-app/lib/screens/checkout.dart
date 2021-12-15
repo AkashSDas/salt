51a163469bb4820cf94e63b5b706559eb86be410
AkashSDas <aakashdas368@gmail.com>
2021-12-15 12:05:31 +0530

    display cart price in checkout screen

--- flutter-app/lib/screens/checkout.dart
+++ flutter-app/lib/screens/checkout.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:salt/services/product.dart';
+import 'package:salt/widgets/drawer/animate_appbar_on_scroll.dart';
+
+import '../design_system.dart';
 
 class CheckoutScreen extends StatelessWidget {
-  const CheckoutScreen({Key? key}) : super(key: key);
+  final _service = ProductService();
+  CheckoutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimateAppBarOnScroll(
+      children: [
+        const SizedBox(height: 20),
+        Text(
+          'Checkout',
+          style: DesignSystem.heading4,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 40),
+        FutureBuilder(
+          future: _service.getTotalCartPrice(),
+          builder: (context, AsyncSnapshot<double> snapshot) {
+            if (!snapshot.hasData) {
+              return Text(
+                'Total amount is 0',
+                style: DesignSystem.bodyMain,
+                textAlign: TextAlign.center,
+              );
+            }
+
+            return Text(
+              'Total amount is \$${snapshot.data ?? 0}',
+              style: DesignSystem.bodyMain,
+              textAlign: TextAlign.center,
+            );
+          },
+        ),
+      ],
+    );
   }
 }
