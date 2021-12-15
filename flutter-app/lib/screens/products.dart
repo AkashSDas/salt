6d85576fe46e38b40e55de5c1c54d779ca178f65
AkashSDas <aakashdas368@gmail.com>
2021-12-15 21:36:56 +0530

    add infinite products scroll in products screen

--- flutter-app/lib/screens/products.dart
+++ flutter-app/lib/screens/products.dart
@@ -1,10 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:salt/providers/animated_drawer.dart';
+import 'package:salt/providers/product_infinite_scroll.dart';
+import 'package:salt/screens/tag.dart';
+import 'package:salt/widgets/common/loader.dart';
+import 'package:salt/widgets/drawer/animated_drawer.dart';
+
+import '../design_system.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedDrawer(
+      child: ChangeNotifierProvider(
+        create: (context) => ProductInfiniteScrollProvider(),
+        child: const _ProductsListView(),
+      ),
+    );
+  }
+}
+
+class _ProductsListView extends StatefulWidget {
+  const _ProductsListView({Key? key}) : super(key: key);
+
+  @override
+  State<_ProductsListView> createState() => _ProductsListViewState();
+}
+
+class _ProductsListViewState extends State<_ProductsListView> {
+  final ScrollController _ctrl = ScrollController();
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      Provider.of<ProductInfiniteScrollProvider>(
+        context,
+        listen: false,
+      ).initialFetch();
+
+      /// Scroll event for fetching more posts
+      _ctrl.addListener(() {
+        var loading = Provider.of<ProductInfiniteScrollProvider>(
+          context,
+          listen: false,
+        ).loading;
+
+        var reachedEnd = Provider.of<ProductInfiniteScrollProvider>(
+          context,
+          listen: false,
+        ).reachedEnd;
+
+        var pixels = _ctrl.position.pixels;
+        var maxScrollExtent = _ctrl.position.maxScrollExtent;
+
+        if (pixels >= maxScrollExtent && !loading && !reachedEnd) {
+          /// fetch more
+          Provider.of<ProductInfiniteScrollProvider>(
+            context,
+            listen: false,
+          ).fetchMore();
+        }
+
+        if (pixels >= 0) {
+          /// Listview has be scrolled (when == 0 you're at top)
+          Provider.of<AnimatedDrawerProvider>(
+            context,
+            listen: false,
+          ).animateAppBar(pixels);
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _provider = Provider.of<ProductInfiniteScrollProvider>(context);
+
+    return ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      controller: _ctrl,
+      children: [
+        _provider.firstLoading ? const SearchLoader() : const SizedBox(),
+        Products(products: _provider.products),
+        _provider.reachedEnd
+            ? Text(
+                "You've reached the end",
+                style: DesignSystem.bodyMain,
+                textAlign: TextAlign.center,
+              )
+            : !_provider.firstLoading
+                ? const SearchLoader()
+                : const SizedBox(),
+      ],
+    );
   }
 }
