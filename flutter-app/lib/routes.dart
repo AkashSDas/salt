17a2f33399dfb7efc98d7f6890653a21906da9d8
AkashSDas <aakashdas368@gmail.com>
2021-12-15 21:37:09 +0530

    create route for products screen

--- flutter-app/lib/routes.dart
+++ flutter-app/lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:salt/screens/app_splash_screen.dart';
 import 'package:salt/screens/checkout.dart';
 import 'package:salt/screens/login.dart';
+import 'package:salt/screens/products.dart';
 import 'package:salt/screens/signup.dart';
 import 'package:salt/screens/user_cart.dart';
 
@@ -12,5 +13,6 @@ Map<String, Widget Function(BuildContext)> getRoutes(BuildContext context) {
     '/auth/login': (context) => const LoginScreen(),
     '/user/cart': (context) => const UserCartScreen(),
     '/user/checkout': (context) => CheckoutScreen(),
+    '/products': (context) => const ProductsScreen(),
   };
 }
