d3bd91490ca9643350ef716e60b911985f54be29
AkashSDas <aakashdas368@gmail.com>
2021-12-16 20:18:30 +0530

    create state, set setup intent and get payment cards in user payment provider

--- flutter-app/lib/providers/user_payment.dart
+++ flutter-app/lib/providers/user_payment.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:salt/services/payment.dart';
 
 class UserPaymentProvider extends ChangeNotifier {
   /// User payment cards saved
   List cards = [];
+  SetupIntent? setupIntent;
+  bool error = false;
+  String msg = '';
 
   var loading = false;
 
@@ -16,5 +21,28 @@ class UserPaymentProvider extends ChangeNotifier {
   void setLoading(bool value) {
     loading = value;
     notifyListeners();
+  }
+
+  /// Update setup intent
+  void setSetupIntent(SetupIntent value) {
+    setupIntent = value;
+    notifyListeners();
+  }
+
+  /// Get user's all payment cards
+  /// The return here is of [bool] type representing the [loading state]
+  Future<bool> getPaymentCards(String userId, String token) async {
+    final service = PaymentService();
+    final data = await service.getUserPaymentCards(userId, token);
+    if (data['error'] || data['cards'] == null) {
+      error = true;
+      msg = data['msg'];
+    } else {
+      if (data['cards'].isNotEmpty) {
+        cards = data['cards'];
+      }
+    }
+    notifyListeners();
+    return true;
   }
 }
