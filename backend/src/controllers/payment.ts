b5e9458d65eefd7dd0bababa99ba934fa75df30b
AkashSDas <aakashdas368@gmail.com>
2021-12-16 11:27:44 +0530

    create user setup intent

--- backend/src/controllers/payment.ts
+++ backend/src/controllers/payment.ts
@@ -1,4 +1,4 @@
-import { listPaymentMethods } from "../payments/customer";
+import { createSetupIntent, listPaymentMethods } from "../payments/customer";
 import { Controller, responseMsg } from "../utils";
 
 /**
@@ -18,5 +18,25 @@ export const getUserPaymentCards: Controller = async (req, res) => {
     status: 200,
     msg: "Successfully retrived user's payment cards",
     data: { cards: cards.data },
+  });
+};
+
+/**
+ * Create setup intent for a user
+ *
+ * @remarks
+ *
+ * Use this in conjunction with
+ * - getUserById which will set `req.profile`
+ */
+export const createUserSetupIntent: Controller = async (req, res) => {
+  const user = req.profile;
+  const data = await createSetupIntent(user._id);
+  if (!data) return responseMsg(res);
+  return responseMsg(res, {
+    error: false,
+    status: 200,
+    msg: "Successfully saved payment card",
+    data: { ...data },
   });
 };
