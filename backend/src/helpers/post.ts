f6db522904900b76f3ac0d5e0e44a75f1bc93ced
AkashSDas <aakashdas368@gmail.com>
2021-12-17 09:25:30 +0530

    create post create form callback

--- backend/src/helpers/post.ts
+++ backend/src/helpers/post.ts
@@ -6,6 +6,95 @@ import {
   deleteFileInFirebaseStorage,
   uploadToFirebaseStorage,
 } from "../firebase";
+import Post from "../models/post";
+
+/**
+ * This funciton is a callback for formidable IncomingForm.parse callback in
+ * **createPost** controller
+ *
+ * @remarks
+ * This callback does the creating work of post data.
+ */
+export const postCreateFormCallback = async (
+  req: Request,
+  res: Response,
+  err: any,
+  fields: Fields,
+  files: Files
+): Promise<void> => {
+  // If user is coming till here after passing all vaidators then chances are
+  // high that the problem is in the file itself
+  if (err) responseMsg(res, { msg: "There is some issue with the file" });
+
+  const user = req.profile;
+
+  // Parse tags
+  try {
+    fields.tags = JSON.parse(fields.tags as string);
+  } catch (er) {
+    return responseMsg(res, { status: 400, msg: "Tags have wrong format" });
+  }
+
+  // Naive method to calc word count and read time
+  const wordCount = (fields.content as string).trim().split(/\s+/g).length;
+  const readTime = parseFloat((wordCount / 100 + 1).toFixed(0));
+
+  let post = new Post({ userId: user._id, ...fields, wordCount, readTime });
+
+  if (!files.coverImg) {
+    return responseMsg(res, {
+      error: true,
+      status: 400,
+      msg: "Cover image is required",
+    });
+  } else {
+    const destination = `post-cover-imgs/${user._id}/${post._id}`;
+
+    // Upload img
+    const url = await uploadToFirebaseStorage(
+      destination,
+      files.coverImg as File,
+      { contentType: "image/png" }
+    );
+    if (url.length === 0) return responseMsg(res);
+    post.coverImgURL = url;
+  }
+
+  const [data, err2] = await runAsync(post.save());
+  if (err2 || !data) return responseMsg(res);
+  const [fullPost, err3] = await runAsync(data.populate("userId tags"));
+  if (err3 || !fullPost) return responseMsg(res);
+  return responseMsg(res, {
+    status: 200,
+    error: false,
+    msg: "Successfully created a post",
+    data: {
+      post: {
+        id: fullPost._id,
+        title: fullPost.title,
+        description: fullPost.description,
+        content: fullPost.content,
+        readTime: fullPost.readTime,
+        wordCount: fullPost.wordCount,
+        published: fullPost.published,
+        coverImgURL: fullPost.coverImgURL,
+        user: {
+          id: fullPost.userId._id,
+          email: fullPost.userId.email,
+          username: fullPost.userId.username,
+          profilePicURL: fullPost.userId.profilePicURL,
+          dateOfBirth: fullPost.userId.dateOfBirth,
+          roles: fullPost.userId.roles,
+        },
+        tags: fullPost.tags.map((tag: any) => ({
+          id: tag._id,
+          emoji: tag.emoji,
+          name: tag.name,
+        })),
+      },
+    },
+  });
+};
 
 /**
  * This function is a callback for formidable IncomingForm.parse callback in
