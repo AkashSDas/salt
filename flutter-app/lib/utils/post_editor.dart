d3e882c22ae35369c5b48d3397b04614b07fb9fe
AkashSDas <aakashdas368@gmail.com>
2021-12-17 14:03:53 +0530

    create validator and create post class

---
+++ flutter-app/lib/utils/post_editor.dart
@@ -0,0 +1,49 @@
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PostFormValidators {
+  final title = MultiValidator([
+    RequiredValidator(errorText: 'Title is required'),
+    MinLengthValidator(
+      6,
+      errorText: 'Title should be atleast 6 characters long',
+    ),
+  ]);
+
+  final description = MultiValidator([
+    RequiredValidator(errorText: 'Description is required'),
+    MinLengthValidator(
+      6,
+      errorText: 'Description should be atleast 6 characters long',
+    ),
+  ]);
+
+  final content = MultiValidator([
+    RequiredValidator(errorText: 'Content is required'),
+    MinLengthValidator(
+      6,
+      errorText: 'Content should be atleast 6 characters long',
+    ),
+  ]);
+}
+
+/// Shape of post that's being created
+class CreatePost {
+  final String title;
+  final String description;
+  final String content;
+  final List<String> categories;
+  final String authorId;
+  final XFile coverImg;
+  final bool published;
+
+  const CreatePost({
+    required this.title,
+    required this.description,
+    required this.content,
+    required this.categories,
+    required this.authorId,
+    required this.coverImg,
+    required this.published,
+  });
+}
