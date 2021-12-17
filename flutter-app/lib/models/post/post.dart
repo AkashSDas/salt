cfa03f538736f4e92c308016e324dbb6b036c35b
AkashSDas <aakashdas368@gmail.com>
2021-12-17 20:08:36 +0530

    create post model

---
+++ flutter-app/lib/models/post/post.dart
@@ -0,0 +1,38 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:salt/models/tag/tag.dart';
+import 'package:salt/models/user/user.dart';
+
+part 'post.g.dart';
+
+@JsonSerializable()
+class Post {
+  final String id;
+  final String title;
+  final String description;
+  final String content;
+  final double readTime;
+  final int wordCount;
+  final bool published;
+  final String coverImgURL;
+  final User user;
+  final List<Tag> tags;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.content,
+    required this.readTime,
+    required this.wordCount,
+    required this.published,
+    required this.coverImgURL,
+    required this.user,
+    required this.tags,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return _$PostFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$PostToJson(this);
+}
---
+++ flutter-app/lib/models/post/post.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Post _$PostFromJson(Map<String, dynamic> json) => Post(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      content: json['content'] as String,
+      readTime: (json['readTime'] as num).toDouble(),
+      wordCount: json['wordCount'] as int,
+      published: json['published'] as bool,
+      coverImgURL: json['coverImgURL'] as String,
+      user: User.fromJson(json['user'] as Map<String, dynamic>),
+      tags: (json['tags'] as List<dynamic>)
+          .map((e) => Tag.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$PostToJson(Post instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'content': instance.content,
+      'readTime': instance.readTime,
+      'wordCount': instance.wordCount,
+      'published': instance.published,
+      'coverImgURL': instance.coverImgURL,
+      'user': instance.user,
+      'tags': instance.tags,
+    };
