c26bc749daa2f5c09718374cd029570c1847055e
AkashSDas <aakashdas368@gmail.com>
2021-12-18 10:26:36 +0530

    solve render overflow issue in groceries covers

--- flutter-app/lib/widgets/product/covers.dart
+++ flutter-app/lib/widgets/product/covers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 // import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 /// There is a [RenderFlex] overflowed by 5.0 pixels on the right error
@@ -9,33 +10,27 @@ class GroceriesCovers extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 284 + 8,
-            child: Column(
-              children: [
-                GroceriesCoverImage(
-                  imgPath: 'dairy-products',
-                  width: MediaQuery.of(context).size.width * 0.5 - 16 - 3,
-                  height: 138,
-                ),
-                const SizedBox(height: 8),
-                GroceriesCoverImage(
-                  imgPath: 'fresh-fruities',
-                  width: MediaQuery.of(context).size.width * 0.5 - 16 - 3,
-                  height: 138,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(width: 6),
-          GroceriesCoverImage(
-            imgPath: 'fresh-veggies',
-            width: MediaQuery.of(context).size.width * 0.5 - 16 - 3,
-            height: 284,
-          ),
-        ],
+      child: StaggeredGridView.countBuilder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+        itemCount: 3,
+        itemBuilder: (context, idx) {
+          double height = 138;
+          if (idx == 1) height = 284;
+          const imgs = ['dairy-products', 'fresh-veggies', 'fresh-fruities'];
+          return GroceriesCoverImage(
+            imgPath: imgs[idx],
+            width: 178,
+            height: height,
+          );
+        },
+        staggeredTileBuilder: (idx) {
+          /// StaggeredTile.count(x, x*h/w)
+          return StaggeredTile.count(1, idx == 1 ? 284 / 178 : 138 / 178);
+        },
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
       ),
     );
   }
@@ -63,7 +58,7 @@ class GroceriesCoverImage extends StatelessWidget {
         borderRadius: BorderRadius.circular(32),
         image: DecorationImage(
           image: AssetImage('assets/images/$imgPath.png'),
-          fit: BoxFit.cover,
+          fit: BoxFit.contain,
         ),
       ),
     );
@@ -74,24 +69,3 @@ class GroceriesCoverImage extends StatelessWidget {
 /// The only issue it faces is the image height (cover/contain) issue. This won't
 /// solve the [RenderFlex] overflowed error on [GroceriesCovers]
 //
-// StaggeredGridView.countBuilder(
-//         shrinkWrap: true,
-//         physics: const NeverScrollableScrollPhysics(),
-//         crossAxisCount: 2,
-//         itemCount: 3,
-//         itemBuilder: (context, idx) {
-//           double height = 138;
-//           if (idx == 1) height = 284;
-//           const imgs = ['dairy-products', 'fresh-veggies', 'fresh-fruities'];
-//           return GroceriesCoverImage(
-//             imgPath: imgs[idx],
-//             width: 178,
-//             height: height,
-//           );
-//         },
-//         staggeredTileBuilder: (idx) {
-//           return StaggeredTile.count(1, idx == 1 ? 2 : 1);
-//         },
-//         mainAxisSpacing: 8,
-//         crossAxisSpacing: 8,
-//       ),
