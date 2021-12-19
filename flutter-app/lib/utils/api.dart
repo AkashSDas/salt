b1c54189ce6abccb0823a01d94f6d68de64a0963
AkashSDas <aakashdas368@gmail.com>
2021-12-19 20:47:57 +0530

    refactor api utils

--- flutter-app/lib/utils/api.dart
+++ flutter-app/lib/utils/api.dart
@@ -1,3 +1,6 @@
+/// Api response for backend is stored in [ApiResponse] class where
+/// the [msg] is msg send, [data] is dynamic and which it will store data
+/// for display in UI, [error] is the status of error
 class ApiResponse {
   String msg;
   bool error;
@@ -6,6 +9,7 @@ class ApiResponse {
   ApiResponse({required this.error, required this.msg, required this.data});
 }
 
+/// Presets for response text to display in things like [snackbar] and others...
 class ApiMessages {
   static const wentWrong = 'Something went wrong';
 }
