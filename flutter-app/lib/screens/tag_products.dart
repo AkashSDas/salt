a963aab7d31d5da96d9f65d1d3143f68dd44af2b
AkashSDas <aakashdas368@gmail.com>
2021-12-21 19:12:49 +0530

    refactor tag products screen

--- flutter-app/lib/screens/tag_products.dart
+++ flutter-app/lib/screens/tag_products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salt/design_system.dart';
 import 'package:salt/models/product/product.dart';
 import 'package:salt/screens/tag.dart';
 import 'package:salt/services/product.dart';
@@ -7,8 +8,6 @@ import 'package:salt/widgets/common/buttons.dart';
 import 'package:salt/widgets/common/cool.dart';
 import 'package:salt/widgets/common/loader.dart';
 import 'package:salt/widgets/drawer/animate_appbar_on_scroll.dart';
-
-import '../design_system.dart';
 
 class TagProductsScreen extends StatelessWidget {
   final _service = ProductService();
@@ -19,6 +18,8 @@ class TagProductsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimateAppBarOnScroll(
       children: [
+        Text('Shop', style: DesignSystem.heading3, textAlign: TextAlign.center),
+        DesignSystem.spaceH40,
         FutureBuilder(
           future: _service.getProductForTag(tagId),
           builder: (context, AsyncSnapshot<ApiResponse> snapshot) {
@@ -33,30 +34,38 @@ class TagProductsScreen extends StatelessWidget {
                 .map((prod) => Product.fromJson(prod))
                 .toList();
 
-            if (products.isEmpty) {
-              return Column(
-                children: [
-                  const LogoTV(),
-                  const SizedBox(height: 20),
-                  Text(
-                    'No products available for this category',
-                    style: DesignSystem.bodyMain,
-                  ),
-                  const SizedBox(height: 20),
-                  PrimaryButton(
-                    text: 'Shop something else',
-                    horizontalPadding: 32,
-                    onPressed: () => Navigator.pushNamed(context, '/products'),
-                  ),
-                ],
-              );
-            }
+            if (products.isEmpty) return const _NoProductsAvailable();
 
             return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              padding: const EdgeInsets.all(16),
               child: Products(products: products),
             );
           },
+        ),
+      ],
+    );
+  }
+}
+
+/// No products available
+class _NoProductsAvailable extends StatelessWidget {
+  const _NoProductsAvailable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const LogoTV(),
+        DesignSystem.spaceH20,
+        Text(
+          'No products available for this category',
+          style: DesignSystem.bodyMain,
+        ),
+        DesignSystem.spaceH20,
+        PrimaryButton(
+          text: 'Shop something else',
+          horizontalPadding: 32,
+          onPressed: () => Navigator.pushNamed(context, '/products'),
         ),
       ],
     );
