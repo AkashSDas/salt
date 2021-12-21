8b647219639dd31803add5673c3a24b04a018adc
AkashSDas <aakashdas368@gmail.com>
2021-12-21 18:11:07 +0530

    refactor login screen

--- flutter-app/lib/screens/login.dart
+++ flutter-app/lib/screens/login.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:iconly/iconly.dart';
 import 'package:provider/provider.dart';
+import 'package:salt/design_system.dart';
 import 'package:salt/providers/animated_drawer.dart';
 import 'package:salt/providers/login_form.dart';
 import 'package:salt/providers/user_provider.dart';
+import 'package:salt/validators.dart';
 import 'package:salt/widgets/common/alert.dart';
 import 'package:salt/widgets/common/buttons.dart';
 import 'package:salt/widgets/common/form.dart';
 import 'package:salt/widgets/drawer/animated_drawer.dart';
-
-import '../design_system.dart';
-import '../validators.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -40,6 +39,9 @@ class __LoginBodyState extends State<_LoginBody> {
     super.initState();
 
     WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      /// The appbar will be at height 64, from where no animation will be
+      /// applied on scroll as there are not much content on screen so no
+      /// scrolling
       Provider.of<AnimatedDrawerProvider>(
         context,
         listen: false,
@@ -52,58 +54,48 @@ class __LoginBodyState extends State<_LoginBody> {
     return ChangeNotifierProvider(
       create: (context) => LoginFormProvider(),
       child: ListView(
-        shrinkWrap: true,
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-        physics: const ClampingScrollPhysics(),
         children: [
-          SizedBox(
-            height: 120,
-            width: 120,
-            child: Builder(
-              builder: (context) {
-                final _p = Provider.of<LoginFormProvider>(context);
-                return FlareActor(
-                  'assets/flare/other-emojis/glasses.flr',
-                  alignment: Alignment.center,
-                  fit: BoxFit.contain,
-                  animation: _p.animation,
-                );
-              },
-            ),
-          ),
+          const Glasses(),
           _EmailInputField(validator: validator.email),
-          const SizedBox(height: 20),
+          DesignSystem.spaceH20,
           _PasswordInputField(validator: validator.password),
-          const SizedBox(height: 40),
+          DesignSystem.spaceH40,
           const _SubmitBtn(),
-          const SizedBox(height: 20),
-          Align(
-            alignment: Alignment.center,
-            child: InkWell(
-              onTap: () => Navigator.pushNamed(context, '/auth/singup'),
-              child: RichText(
-                text: TextSpan(
-                    text: "Don't have an ",
-                    style: DesignSystem.caption,
-                    children: [
-                      TextSpan(
-                        text: 'account',
-                        style: DesignSystem.caption.copyWith(
-                          color: Colors.blue,
-                        ),
-                      ),
-                      const TextSpan(text: '?'),
-                    ]),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
+          DesignSystem.spaceH20,
+          _buildSignupHelperText(),
+          DesignSystem.spaceH20,
         ],
+      ),
+    );
+  }
+
+  Widget _buildSignupHelperText() {
+    return Align(
+      alignment: Alignment.center,
+      child: InkWell(
+        onTap: () => Navigator.pushNamed(context, '/auth/singup'),
+        child: RichText(
+          text: TextSpan(
+            text: "Don't have an ",
+            style: DesignSystem.caption,
+            children: [
+              TextSpan(
+                text: 'account',
+                style: DesignSystem.caption.copyWith(
+                  color: Colors.blue,
+                ),
+              ),
+              const TextSpan(text: '?'),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
+/// Submit btn
 class _SubmitBtn extends StatelessWidget {
   const _SubmitBtn({Key? key}) : super(key: key);
 
@@ -111,6 +103,7 @@ class _SubmitBtn extends StatelessWidget {
   Widget build(BuildContext context) {
     final _provider = Provider.of<LoginFormProvider>(context);
     final _user = Provider.of<UserProvider>(context);
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 64),
       child: PrimaryButton(
@@ -135,17 +128,15 @@ class _SubmitBtn extends StatelessWidget {
   }
 }
 
+/// Email input
 class _EmailInputField extends StatelessWidget {
   final MultiValidator validator;
-
-  const _EmailInputField({
-    required this.validator,
-    Key? key,
-  }) : super(key: key);
+  const _EmailInputField({required this.validator, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final _provider = Provider.of<LoginFormProvider>(context);
+
     return IconFormInput(
       prefixIcon: const Icon(IconlyLight.message, color: DesignSystem.icon),
       label: 'Email',
@@ -159,6 +150,8 @@ class _EmailInputField extends StatelessWidget {
     );
   }
 }
+
+/// Password input
 
 class _PasswordInputField extends StatefulWidget {
   final MultiValidator validator;
@@ -178,6 +171,7 @@ class _PasswordInputFieldState extends State<_PasswordInputField> {
   @override
   Widget build(BuildContext context) {
     final _provider = Provider.of<LoginFormProvider>(context);
+
     return IconFormInput(
       prefixIcon: const Icon(IconlyLight.lock, color: DesignSystem.icon),
       suffixIcon: InkWell(
@@ -194,6 +188,27 @@ class _PasswordInputFieldState extends State<_PasswordInputField> {
       hintText: 'Secure password',
       validator: widget.validator,
       obscureText: showPassword ? false : true,
+    );
+  }
+}
+
+/// Glasses flare actor
+class Glasses extends StatelessWidget {
+  const Glasses({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _p = Provider.of<LoginFormProvider>(context);
+
+    return SizedBox(
+      height: 120,
+      width: 120,
+      child: FlareActor(
+        'assets/flare/other-emojis/glasses.flr',
+        alignment: Alignment.center,
+        fit: BoxFit.contain,
+        animation: _p.animation,
+      ),
     );
   }
 }
