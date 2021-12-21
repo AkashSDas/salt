880d45d2cd60f60414d8c536801f8b4badc863e8
AkashSDas <aakashdas368@gmail.com>
2021-12-21 18:23:18 +0530

    refactor post screen

--- flutter-app/lib/screens/post.dart
+++ flutter-app/lib/screens/post.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:salt/design_system.dart';
 import 'package:salt/models/post/post.dart';
 import 'package:salt/models/tag/tag.dart';
 import 'package:salt/screens/product.dart';
+import 'package:salt/screens/tag.dart';
 import 'package:salt/widgets/drawer/animate_appbar_on_scroll.dart';
 import 'package:salt/widgets/post/big_post.dart';
-
-import '../design_system.dart';
 
 class PostScreen extends StatelessWidget {
   final Post post;
@@ -21,20 +21,21 @@ class PostScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(post.title, style: DesignSystem.heading3),
-              const SizedBox(height: 20),
+              DesignSystem.spaceH20,
               Text(post.description, style: DesignSystem.bodyIntro),
-              const SizedBox(height: 20),
+              DesignSystem.spaceH20,
               PostMetadata(
                 authorProfilePicURL: post.user.profilePicURL,
                 authorName: post.user.username,
                 updatedAt: post.updatedAt,
               ),
-              const SizedBox(height: 20),
+              DesignSystem.spaceH20,
               BigPostCoverImg(url: post.coverImgURL),
-              const SizedBox(height: 20),
+              DesignSystem.spaceH20,
               TagsInlineView(tags: post.tags),
-              const SizedBox(height: 20),
+              DesignSystem.spaceH20,
               MarkdownContent(text: post.content),
+              DesignSystem.spaceH40,
             ],
           ),
         )
@@ -43,6 +44,7 @@ class PostScreen extends StatelessWidget {
   }
 }
 
+/// Display [tags] in horizontal scroll view
 class TagsInlineView extends StatelessWidget {
   final List<Tag> tags;
   const TagsInlineView({required this.tags, Key? key}) : super(key: key);
@@ -55,18 +57,28 @@ class TagsInlineView extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: tags.length,
         itemBuilder: (context, idx) {
-          return Container(
-            height: 44,
-            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-            margin: EdgeInsets.only(right: idx == tags.length - 1 ? 0 : 16),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              border: Border.all(color: DesignSystem.border),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Text(
-              '${tags[idx].emoji} ${tags[idx].name[0].toUpperCase()}${tags[idx].name.substring(1)}',
-              style: DesignSystem.caption,
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TagScreen(tagId: tags[idx].id),
+                ),
+              );
+            },
+            child: Container(
+              height: 44,
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+              margin: EdgeInsets.only(right: idx == tags.length - 1 ? 0 : 16),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(color: DesignSystem.border),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Text(
+                '${tags[idx].emoji} ${tags[idx].name[0].toUpperCase()}${tags[idx].name.substring(1)}',
+                style: DesignSystem.caption,
+              ),
             ),
           );
         },
