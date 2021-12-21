d4c79795ce19f78bdcaf1bf5b26f2defa4b4410e
AkashSDas <aakashdas368@gmail.com>
2021-12-21 18:48:43 +0530

    refactor product screen

--- flutter-app/lib/screens/product.dart
+++ flutter-app/lib/screens/product.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:provider/provider.dart';
 import 'package:salt/design_system.dart';
 import 'package:salt/models/product/product.dart';
@@ -8,6 +7,7 @@ import 'package:salt/providers/user_provider.dart';
 import 'package:salt/services/product.dart';
 import 'package:salt/widgets/common/alert.dart';
 import 'package:salt/widgets/common/buttons.dart';
+import 'package:salt/widgets/common/markdown.dart';
 import 'package:salt/widgets/drawer/animate_appbar_on_scroll.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -20,19 +20,27 @@ class ProductScreen extends StatelessWidget {
       create: (context) => ProductProvider(price: product.price),
       child: AnimateAppBarOnScroll(
         children: [
-          const SizedBox(height: 20),
+          DesignSystem.spaceH20,
           _CoverImgsSlideshow(imgs: product.coverImgURLs),
-          const SizedBox(height: 20),
+          DesignSystem.spaceH20,
           Padding(
             padding: const EdgeInsets.all(16).copyWith(top: 0),
             child: Column(
               children: [
                 Text(product.title, style: DesignSystem.heading4),
-                const SizedBox(height: 20),
-                MarkdownContent(text: product.description),
-                const SizedBox(height: 20),
-                MarkdownContent(text: product.info),
-                const SizedBox(height: 20),
+                DesignSystem.spaceH20,
+                MarkdownContent(
+                  text: product.description,
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                ),
+                DesignSystem.spaceH20,
+                MarkdownContent(
+                  text: product.info,
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                ),
+                DesignSystem.spaceH20,
                 ProductToCart(product: product),
               ],
             ),
@@ -43,6 +51,8 @@ class ProductScreen extends StatelessWidget {
   }
 }
 
+/// Product price and quantity section
+
 class ProductToCart extends StatefulWidget {
   final Product product;
   const ProductToCart({required this.product, Key? key}) : super(key: key);
@@ -52,91 +62,120 @@ class ProductToCart extends StatefulWidget {
 }
 
 class _ProductToCartState extends State<ProductToCart> {
+  Widget _buildPrice(BuildContext context) {
+    final _provider = Provider.of<ProductProvider>(context);
+
+    return Text(
+      _provider.displayPrice.toString(),
+      style: const TextStyle(
+        color: DesignSystem.success,
+        fontFamily: DesignSystem.fontHighlight,
+        fontWeight: FontWeight.w400,
+        fontSize: 40,
+      ),
+    );
+  }
+
+  Widget _buildQuantityUpdateBtns(BuildContext context) {
+    final _provider = Provider.of<ProductProvider>(context);
+
+    var decrementIcon = IconButton(
+      onPressed: () {
+        if (_provider.quantity > 1) _provider.updateProductQuantity(-1);
+      },
+      icon: const Icon(Icons.remove),
+    );
+
+    var space = const SizedBox(width: 16);
+
+    var incrementIcon = IconButton(
+      onPressed: () {
+        if (_provider.quantity < widget.product.quantityLeft) {
+          _provider.updateProductQuantity(1);
+        }
+      },
+      icon: const Icon(Icons.add),
+    );
+
+    return Row(
+      children: [
+        decrementIcon,
+        space,
+        Text(
+          _provider.quantity.toString(),
+          style: DesignSystem.bodyIntro.copyWith(
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        space,
+        incrementIcon,
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _provider = Provider.of<ProductProvider>(context);
-    final _user = Provider.of<UserProvider>(context);
-
     return Column(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              _provider.displayPrice.toString(),
-              style: const TextStyle(
-                color: DesignSystem.success,
-                fontFamily: DesignSystem.fontHighlight,
-                fontWeight: FontWeight.w400,
-                fontSize: 40,
-              ),
-            ),
+            _buildPrice(context),
             Container(
               padding: const EdgeInsets.symmetric(vertical: 6),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(32),
                 color: DesignSystem.card,
               ),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      if (_provider.quantity > 1) {
-                        _provider.updateProductQuantity(-1);
-                      }
-                    },
-                    icon: const Icon(Icons.remove),
-                  ),
-                  const SizedBox(width: 16),
-                  Text(
-                    _provider.quantity.toString(),
-                    style: DesignSystem.bodyIntro.copyWith(
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  IconButton(
-                    onPressed: () {
-                      if (_provider.quantity < widget.product.quantityLeft) {
-                        _provider.updateProductQuantity(1);
-                      }
-                    },
-                    icon: const Icon(Icons.add),
-                  ),
-                ],
-              ),
+              child: _buildQuantityUpdateBtns(context),
             ),
           ],
         ),
-        const SizedBox(height: 20),
-        PrimaryButton(
-          text: _provider.loading ? 'Saving...' : 'Add to cart',
-          onPressed: () async {
-            if (_user.token == null) {
-              failedSnackBar(context: context, msg: 'You are not logged in');
-              Navigator.pushNamed(context, '/auth/login');
-            } else {
-              final _service = ProductService();
-              _provider.updateLoading(true);
-              var response = await _service.saveProductToCart({
-                ...widget.product.toJson(),
-                'quantitySelected': _provider.quantity,
-              });
-              _provider.updateLoading(false);
-
-              if (response['error']) {
-                failedSnackBar(context: context, msg: response['msg']);
-              } else {
-                successSnackBar(context: context, msg: response['msg']);
-              }
-            }
-          },
-          horizontalPadding: 64,
-        ),
+        DesignSystem.spaceH20,
+        _AddToCartButton(product: widget.product),
       ],
     );
   }
 }
+
+/// Add to cart btn
+class _AddToCartButton extends StatelessWidget {
+  final Product product;
+  const _AddToCartButton({required this.product, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _provider = Provider.of<ProductProvider>(context);
+    final _user = Provider.of<UserProvider>(context);
+
+    return PrimaryButton(
+      text: _provider.loading ? 'Saving...' : 'Add to cart',
+      onPressed: () async {
+        if (_user.token == null) {
+          failedSnackBar(context: context, msg: 'You are not logged in');
+          Navigator.pushNamed(context, '/auth/login');
+        } else {
+          final _service = ProductService();
+          _provider.updateLoading(true);
+          var response = await _service.saveProductToCart({
+            ...product.toJson(),
+            'quantitySelected': _provider.quantity,
+          });
+          _provider.updateLoading(false);
+
+          if (response['error']) {
+            failedSnackBar(context: context, msg: response['msg']);
+          } else {
+            successSnackBar(context: context, msg: response['msg']);
+          }
+        }
+      },
+      horizontalPadding: 64,
+    );
+  }
+}
+
+/// Cover img carousel
 
 class _CoverImgsSlideshow extends StatefulWidget {
   final List<String> imgs;
@@ -149,99 +188,87 @@ class _CoverImgsSlideshow extends StatefulWidget {
 class _CoverImgsSlideshowState extends State<_CoverImgsSlideshow> {
   int imgIdx = 0;
 
+  Widget _buildCoverImg() {
+    return Container(
+      height: 250,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(widget.imgs[imgIdx]),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+
+  Widget _buildCarousel() {
+    return SizedBox(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.imgs.length,
+        itemBuilder: (context, idx) {
+          return _buildCarouselImgCard(idx);
+        },
+      ),
+    );
+  }
+
+  Widget _buildCarouselImgCard(int idx) {
+    var current = imgIdx == idx;
+
+    return InkWell(
+      onTap: () => setState(() => imgIdx = idx),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          _buildCarouselImgCardBorder(idx, current),
+          Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.only(
+              right: idx + 1 == widget.imgs.length ? 0 : 10,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: DesignSystem.border,
+                width: current ? 1.5 : 0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCarouselImgCardBorder(int idx, bool current) {
+    return Container(
+      height: current ? 90 : 100,
+      width: current ? 90 : 100,
+      margin: EdgeInsets.only(
+        right: idx + 1 == widget.imgs.length ? 0 : 10,
+      ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(widget.imgs[idx]),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(current ? 16 : 20),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(widget.imgs[imgIdx]),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.imgs.length,
-              itemBuilder: (context, idx) {
-                var current = imgIdx == idx;
-
-                return InkWell(
-                  onTap: () {
-                    setState(() {
-                      imgIdx = idx;
-                    });
-                  },
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        height: current ? 90 : 100,
-                        width: current ? 90 : 100,
-                        margin: EdgeInsets.only(
-                          right: idx + 1 == widget.imgs.length ? 0 : 10,
-                        ),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(widget.imgs[idx]),
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius:
-                              BorderRadius.circular(current ? 16 : 20),
-                        ),
-                      ),
-                      Container(
-                        height: 100,
-                        width: 100,
-                        margin: EdgeInsets.only(
-                          right: idx + 1 == widget.imgs.length ? 0 : 10,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: DesignSystem.border,
-                            width: current ? 1.5 : 0,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          )
+          _buildCoverImg(),
+          DesignSystem.spaceH20,
+          _buildCarousel(),
         ],
-      ),
-    );
-  }
-}
-
-class MarkdownContent extends StatelessWidget {
-  final String text;
-  const MarkdownContent({required this.text, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Markdown(
-      padding: const EdgeInsets.all(0),
-      shrinkWrap: true,
-      physics: const ClampingScrollPhysics(),
-      data: text,
-      styleSheet: MarkdownStyleSheet(
-        h1: DesignSystem.heading1,
-        h2: DesignSystem.heading2,
-        h3: DesignSystem.heading3,
-        h4: DesignSystem.heading4,
-        p: DesignSystem.bodyMain,
-        listBullet: const TextStyle(color: DesignSystem.text2),
       ),
     );
   }
