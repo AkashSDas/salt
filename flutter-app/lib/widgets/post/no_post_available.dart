1a85f0795f203ed9addb3a0da438ad6e86c765a4
AkashSDas <aakashdas368@gmail.com>
2021-12-24 20:48:24 +0530

    create no post available widget

---
+++ flutter-app/lib/widgets/post/no_post_available.dart
@@ -0,0 +1,33 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+import '../../design_system.dart';
+
+class NoPostAvailable extends StatelessWidget {
+  const NoPostAvailable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Center(
+          child: SizedBox(
+            height: 120,
+            width: 120,
+            child: FlareActor(
+              'assets/flare/other-emojis/smiling-face-with-sunglasses.flr',
+              alignment: Alignment.center,
+              fit: BoxFit.contain,
+              animation: 'go',
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'No posts available',
+          style: DesignSystem.bodyIntro,
+        ),
+      ],
+    );
+  }
+}
