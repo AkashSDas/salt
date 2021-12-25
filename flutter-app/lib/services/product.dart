dbd8b955843d3313ca7745f998f0e78f48efb7f3
AkashSDas <aakashdas368@gmail.com>
2021-12-25 14:08:16 +0530

    create get related products service

--- flutter-app/lib/services/product.dart
+++ flutter-app/lib/services/product.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:salt/models/cart_product.dart/cart_product.dart';
+import 'package:salt/models/tag/tag.dart';
 import 'package:salt/utils/api.dart';
 import 'package:salt/utils/index.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart' as storage;
@@ -253,6 +254,90 @@ class ProductService {
       url = '$baseURL?limit=$limit&next=$nextId';
     }
     var res = await runAsync(Dio().get(url, options: options));
+
+    if (res[0] == null) {
+      return ApiResponse(error: true, msg: ApiMessages.wentWrong, data: null);
+    }
+
+    Response apiRes = res[0] as Response;
+    var result = apiRes.data;
+    return ApiResponse(
+      error: result['error'],
+      msg: result['msg'],
+      data: result['data'],
+    );
+  }
+
+  /// Get products related to a tag
+  ///
+  /// If tags is empty then latest [limit] product will be displayed.
+  /// If tags is not empty then the first tagId from tags will be used to
+  /// get latest [limit] products which have that first tag, if in this case the
+  /// tag has no products then it will revert back to getting the latest [limit]
+  /// products that can be any tag.
+  ///
+  /// This service should be used get [limit] related products and if you want
+  /// all products having certain tag (as here this service is getting [limit] products
+  /// for the first tag in [tags], if available)
+  Future<ApiResponse> getRelatedProducts(List<Tag> tags, int limit) async {
+    /// URL for getting latest products `with any tag`
+    var anyProductsURL = '$baseURL?limit=$limit';
+
+    /// URL for getting latest products `with the tags[0]`
+    String? tagProductsURL;
+    if (tags.isNotEmpty) {
+      tagProductsURL = '$baseURL/tag/${tags[0].id}?limit=$limit';
+    }
+
+    /// Check if `tag has any post`
+    if (tagProductsURL != null) {
+      var checkResponse = await runAsync(
+        Dio().get('$baseURL/tag/${tags[0].id}?limit=1', options: options),
+      );
+
+      if (checkResponse[0] == null) {
+        return ApiResponse(error: true, msg: ApiMessages.wentWrong, data: null);
+      }
+
+      Response checkRes = checkResponse[0] as Response;
+      var checkResult = checkRes.data;
+
+      if (checkResult['data'] == null) {
+        /// This means that `tag[0]` has no product
+        /// This won't return anything that the control flow will
+        /// go to getting latest products `with any tag`
+      } else {
+        /// This means that `tag[0]` has products
+
+        var tagResponse = await runAsync(
+          Dio().get(
+            '$baseURL/tag/${tags[0].id}?limit=$limit',
+            options: options,
+          ),
+        );
+
+        if (tagResponse[0] == null) {
+          return ApiResponse(
+            error: true,
+            msg: ApiMessages.wentWrong,
+            data: null,
+          );
+        }
+
+        Response tagRes = tagResponse[0] as Response;
+        var tagResult = tagRes.data;
+
+        return ApiResponse(
+          error: tagResult['error'],
+          msg: tagResult['msg'],
+          data: tagResult['data'],
+        );
+      }
+    }
+
+    /// Getting latest products `with any tag`
+
+    var res = await runAsync(Dio().get(anyProductsURL, options: options));
 
     if (res[0] == null) {
       return ApiResponse(error: true, msg: ApiMessages.wentWrong, data: null);
