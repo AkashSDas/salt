9e0e45559285d794ab1893b0c83401a95dff5bd8
AkashSDas <aakashdas368@gmail.com>
2021-12-25 21:13:26 +0530

    create product order infinite scroll provider

---
+++ flutter-app/lib/providers/product_order_infinite_scroll.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:salt/models/product_order/product_order.dart';
+import 'package:salt/services/product_order.dart';
+
+class ProductOrderInfiniteScrollProvider extends ChangeNotifier {
+  var _disposed = false;
+
+  @override
+  void dispose() {
+    _disposed = true;
+    super.dispose();
+  }
+
+  @override
+  void notifyListeners() {
+    if (!_disposed) {
+      super.notifyListeners();
+    }
+  }
+
+  var limit = 10;
+  List<ProductOrder> orders = [];
+  var loading = false;
+  var reachedEnd = false;
+
+  /// The firstLoading will be used for the first data fetching when
+  /// the widget has mounted and then won't be used again during the
+  /// lifecycle of that widget
+  var firstLoading = false;
+  var firstError = false;
+  var firstApiResponseMsg = '';
+
+  /// Info about next set of orders to fetch from backend
+  var hasNext = false;
+  var nextId = '';
+
+  /// ERROR & MSG
+  var error = false;
+  var apiResponseMsg = '';
+
+  ProductOrderInfiniteScrollProvider({int limit = 2}) {
+    limit = limit;
+  }
+
+  // FETCHING DATA
+
+  Future<void> initialFetchUserProductOrders(
+    String userId,
+    String token,
+    int? limit,
+    String? nextId,
+  ) async {
+    var _service = ProductOrderService();
+
+    setFirstLoading(true);
+    var response = await _service.getUserProductOrders(
+      userId,
+      token,
+      limit,
+      nextId,
+    );
+    setFirstLoading(false);
+
+    /// Checking for error
+    if (response.error) {
+      firstError = true;
+      firstApiResponseMsg = response.msg;
+    } else {
+      List<ProductOrder> newOrders = [];
+      for (int i = 0; i < response.data['orders'].length; i++) {
+        final order = response.data['orders'][i];
+
+        newOrders.add(
+          ProductOrder.fromJson({
+            'id': order['order']['id'],
+            'price': order['order']['price'],
+            'quantity': order['order']['quantity'],
+            'feedback': order['feedback'],
+            'product': order['order']['product'],
+          }),
+        );
+      }
+
+      orders = [...orders, ...newOrders];
+      nextId = response.data['next'];
+      reachedEnd = !response.data['hasNext'];
+    }
+
+    notifyListeners();
+  }
+
+  Future<void> fetchMoreUserProductOrders(String userId, String token) async {
+    var _service = ProductOrderService();
+
+    setLoading(true);
+    var response = await _service.getUserProductOrders(
+      userId,
+      token,
+      limit,
+      nextId,
+    );
+    setLoading(false);
+
+    /// Checking for error
+    if (response.error) {
+      error = true;
+      apiResponseMsg = response.msg;
+    } else {
+      List<ProductOrder> newOrders = [];
+      for (int i = 0; i < response.data['orders'].length; i++) {
+        final order = response.data['orders'][i];
+
+        newOrders.add(
+          ProductOrder.fromJson({
+            'id': order['order']['id'],
+            'price': order['order']['price'],
+            'quantity': order['order']['quantity'],
+            'feedback': order['feedback'],
+            'product': order['order']['product'],
+          }),
+        );
+      }
+
+      orders = [...orders, ...newOrders];
+      nextId = response.data['next'];
+      reachedEnd = !response.data['hasNext'];
+    }
+
+    notifyListeners();
+  }
+
+  /// LOADERS
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
+  }
+
+  void setFirstLoading(bool value) {
+    firstLoading = value;
+    notifyListeners();
+  }
+}
