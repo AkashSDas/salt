699b81715c65cb6b80e94573dbb0c633b10e31c5
AkashSDas <aakashdas368@gmail.com>
2021-12-26 13:15:15 +0530

    create get product feedback overview and get product feedbacks services

--- flutter-app/lib/services/feedback.dart
+++ flutter-app/lib/services/feedback.dart
@@ -132,4 +132,66 @@ class FeedbackService {
       data: null,
     );
   }
+
+  /// Product feedback overview
+  Future<ApiResponse> getProductFeedbackOverview(String productId) async {
+    var res = await runAsync(
+      Dio().get('$baseURL/$productId/overview', options: options),
+    );
+
+    if (res[1] != null) {
+      return ApiResponse(
+        error: true,
+        msg: 'Something went wrong, Please try again',
+        data: null,
+      );
+    }
+
+    var response = res[0] as Response;
+    var data = response.data;
+    if (data['error']) {
+      return ApiResponse(
+        error: true,
+        msg: data['msg'],
+        data: null,
+      );
+    }
+
+    return ApiResponse(
+      error: false,
+      msg: data['msg'],
+      data: data['data'],
+    );
+  }
+
+  /// Product feedbacks
+  Future<ApiResponse> getProductFeedbacks(String productId) async {
+    var res = await runAsync(
+      Dio().get('$baseURL/$productId', options: options),
+    );
+
+    if (res[1] != null) {
+      return ApiResponse(
+        error: true,
+        msg: 'Something went wrong, Please try again',
+        data: null,
+      );
+    }
+
+    var response = res[0] as Response;
+    var data = response.data;
+    if (data['error']) {
+      return ApiResponse(
+        error: true,
+        msg: data['msg'],
+        data: null,
+      );
+    }
+
+    return ApiResponse(
+      error: false,
+      msg: data['msg'],
+      data: data['data'],
+    );
+  }
 }
