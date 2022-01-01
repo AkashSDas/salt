334762cf6f0ce0ecc2b4c61c220a0a9fd0811f63
AkashSDas <aakashdas368@gmail.com>
2022-01-01 14:49:36 +0530

    refactor typo

--- dummy-data/src/user/login.ts
+++ dummy-data/src/user/login.ts
@@ -1,9 +1,9 @@
 import { fetchFromAPI } from "../utils";
 
 export const loginUser = async (email: string) => {
-  const resposne = await fetchFromAPI("/auth/login", {
+  const response = await fetchFromAPI("/auth/login", {
     method: "POST",
     data: { email, password: "testing" },
   });
-  return resposne[0].data.data;
+  return response[0].data.data;
 };
