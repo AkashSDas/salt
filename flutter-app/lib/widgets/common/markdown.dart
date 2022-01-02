a38e52227d6ae85e1ca0c703842351d1c5154ddd
AkashSDas <aakashdas368@gmail.com>
2022-01-02 12:58:10 +0530

    update markdown styling

--- flutter-app/lib/widgets/common/markdown.dart
+++ flutter-app/lib/widgets/common/markdown.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:salt/design_system.dart';
+import 'package:salt/widgets/common/divider.dart';
 
 class MarkdownContent extends StatelessWidget {
   final String text;
@@ -24,11 +25,24 @@ class MarkdownContent extends StatelessWidget {
       physics: physics,
       data: text,
       styleSheet: MarkdownStyleSheet(
-        h1: DesignSystem.heading1,
-        h2: DesignSystem.heading2,
-        h3: DesignSystem.heading3,
-        h4: DesignSystem.heading4,
-        p: DesignSystem.bodyIntro,
+        h1: DesignSystem.heading2,
+        h2: DesignSystem.heading3,
+        h3: DesignSystem.heading4,
+        h4: DesignSystem.bodyIntro.copyWith(color: DesignSystem.text1),
+        p: DesignSystem.bodyMain,
+        blockquoteDecoration: BoxDecoration(
+          color: DesignSystem.card,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        blockSpacing: 20,
+        listBullet: DesignSystem.bodyMain,
+        code: DesignSystem.bodyMain.copyWith(
+          color: DesignSystem.secondary,
+        ),
+        horizontalRuleDecoration: ShapeDecoration(
+          color: DesignSystem.border,
+          shape: Border.all(color: DesignSystem.border),
+        ),
       ),
     );
   }
