f6b3cc86e07d695c9a59c1cb87ad3cf276f313c1
AkashSDas <aakashdas368@gmail.com>
2022-01-02 11:12:59 +0530

    create tag ids util class

---
+++ flutter-app/lib/utils/tags.dart
@@ -0,0 +1,24 @@
+class TagMongoDBIds {
+  static const recipe = '61d01f043af7c60feb69d289';
+  static const cake = '61d01f043af7c60feb69d28d';
+  static const chocolate = '61d01f053af7c60feb69d291';
+  static const fastFood = '61d01f053af7c60feb69d295';
+  static const sweet = '61d01f053af7c60feb69d299';
+  static const salad = '61d01f053af7c60feb69d29d';
+  static const nonVeg = '61d01f063af7c60feb69d2a1';
+  static const highProtein = '61d01f063af7c60feb69d2a5';
+  static const movieSnack = '61d01f063af7c60feb69d2a9';
+  static const lunch = '61d01f063af7c60feb69d2ad';
+  static const sushi = '61d01f073af7c60feb69d2b1';
+  static const dairy = '61d01f073af7c60feb69d2b5';
+  static const seaFood = '61d01f073af7c60feb69d2b9';
+  static const iceCream = '61d01f083af7c60feb69d2bd';
+  static const snack = '61d01f083af7c60feb69d2c1';
+  static const fruits = '61d01f083af7c60feb69d2c5';
+  static const greenVeggies = '61d01f083af7c60feb69d2c9';
+  static const drinks = '61d01f093af7c60feb69d2cd';
+  static const breakfast = '61d01f093af7c60feb69d2d1';
+  static const dinner = '61d01f093af7c60feb69d2d5';
+  static const kitchen = '61d030883af7c60feb69d2db';
+  static const coffee = '61d031273af7c60feb69d2df';
+}
