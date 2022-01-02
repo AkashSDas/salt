a81608e0953c5a1d1ef9e5b2348deac24e7de9ee
AkashSDas <aakashdas368@gmail.com>
2022-01-02 11:11:25 +0530

    print retrieved tags

--- dummy-data/src/tag/get_tags.ts
+++ dummy-data/src/tag/get_tags.ts
@@ -2,5 +2,9 @@ import { fetchFromAPI } from "../utils";
 
 export const getTags = async () => {
   const response = await fetchFromAPI("/tag", { method: "GET" });
-  return response[0].data.data.tags;
+  const tags = response[0].data.data.tags;
+  console.log(tags);
+  return tags;
 };
+
+getTags();
