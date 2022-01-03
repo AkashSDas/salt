542701a5200b844e10998b24ed7c1d6e2308d6f1
AkashSDas <aakashdas368@gmail.com>
2022-01-03 09:12:26 +0530

    create post and product data

---
+++ dummy-data/src/post/data.ts
@@ -0,0 +1,271 @@
+export const breakfastTitles = [
+  "Jammy Eggs and Yogurt Breakfast Bowl Recipe",
+  "Teishoku Breakfast: How to Make a Traditional Japanese Breakfast Set",
+  "Soft Scrambled Eggs and Spicy Chorizo Breakfast Nachos",
+  "Sunday Brunch: Breakfast Yorkshire Puddings with Roast Beef and Eggs",
+  "Sunday Brunch: Breakfast Grilled Cheese",
+  "How to Make French Toast",
+  "Texas Toast",
+  "Monkey Bread",
+  "Classic Chinese Tomato Egg Stir Fry",
+];
+
+export const breakfastDescriptions = [
+  "The meal prep train isn't lost in the age of WFM, it's just changed to breakfast.",
+  "A guide to cooking the perfect teishoku Japanese breakfast set: rice, salmon, miso soup, pickles, and tamagoyaki.",
+  "Yup, you can eat nachos for breakfast, especially when they're topped with soft scrambled eggs, spicy chorizo, and ALL THE CHEESE you can handle.",
+  "If someone asks if you want brunch, the answer should always be: YES.",
+  "It's Sunday! You know what that means: it's time for Sunday Brunch. Why don't you skip the line and make brunch at home this week? The coffee’s truly bottomless, the booze doesn’t have a crazy markup and you can chill out in your pajamas. Every Sunday I’ll post a brunch recipe. Soon you won’t be asking, where should we go for brunch - instead it'll be, what should we make for brunch today?",
+  "Everything about fluffy french toast makes me incredibly happy.",
+  "Texas toast is pure carb pleasure.",
+  "A ring of little balls of chewy, fluffy cinnamon-sugar goodness.",
+  "A simple homestyle Chinese classic: perfectly soft scrambled eggs contrasted with chunky sweet garlicky tart tomatoes.",
+];
+
+export const cakeTitles = [
+  "My Perfect Carrot Cake Recipe",
+  "Cereal Pancakes - Tiny Mini Pancakes!",
+  "The Best Chocolate Mug Cake",
+  "Oreo Cheesecake Recipe",
+  "The Best Japanese Cheesecake Recipe",
+];
+
+export const cakeDescription = [
+  "Just the right amount of spice, with a super moist and tight velvety crumb and the smoothest whipped mascarpone cheesecake frosting.",
+  "Pancakes, but make it MINI! These super cute tiny pancakes are the cutest quarantine breakfast you’ll ever make and eat!",
+  "Mug cake is just so incredibly cozy!",
+  "If you love cookies and cream, you’ll love these no bake Oreo cheesecake bars.",
+  "The Elusive Japanese Mr. Cheesecake: Tokyo’s No. 1 Cheesecake",
+];
+
+export const chocolateTitles = [
+  "TikTok Hot Chocolate Bombs",
+  "Best Brown Butter Chocolate Chip Cookies",
+  "How to make TikTok Whipped Hot Chocolate",
+  "The Best Chocolate Mug Cake",
+  "Small Batch Mini Chocolate Sourdough Babka Loaf",
+  "Super Chocolatey Extra Small Batch Brownies",
+  "Small Batch Chocolate Guinness Cake Loaf",
+  "Vegan Baked Chocolate Cake Doughnuts",
+];
+
+export const chocolateDescription = [
+  "When you pour hot milk over these hot chocolate bombs, they melt and magically release the marshmallows and cocoa hiding inside.",
+  "These rippled salted brown butter chocolate chip cookies are to die for: soft and chewy with pools of dark chocolate, crisp edges, and the salty nuttiness of browned butter. No mixer needed – it’s going to be your new fave!",
+  "TikTok whipped hot chocolate coming in hot for all those fall feels!",
+  "Mug cake is just so incredibly cozy!",
+  "Glossy swirls of chocolate, buttery sweet sourdough brioche, all tucked into the tiniest babka loaf you’ll ever see!",
+  "The perfect tiny small batch brownie recipe when you’re craving just 1 (or four) brownies. Just the right size for shelter-in-place!",
+  "Your next St. Patrick's Day dessert is here! Dark beer and chocolate are a match made in cake heaven.",
+  "These sweet little baked chocolate doughnuts are soft, chocolatey, and super easy to whip up for a quick treat.",
+];
+
+export const coffeeTitles = [
+  "Vietnamese Coffee",
+  "Dalgona Coffee Recipe",
+  "Small Batch Copycat Cinnamon Streusel Starbucks Coffee Cake Recipe",
+  "Coffee Jelly Recipe for Boba",
+  "Mint Mojito Pourover Coffee",
+  "Vietnamese Irish Coffee Recipe",
+];
+
+export const coffeeDescription = [
+  "Sweet and creamy, with a huge kick of intense coffee flavor, Vietnamese coffee is the caffeine hit I want all day, every day.",
+  "A three ingredient, pantry friendly way to have a picture perfect coffee when you can’t go out to a cute cafe.",
+  "Cinnamon streusel crumbs swirled throughout a classic yellow cake - the perfect little loaf to have with coffee.",
+  "If you love coffee jelly in your boba drinks, you've come to the right place: make your own coffee jelly boba at home.",
+  "A riff off of Philz Mint Mojito Coffee. Minty, sweet and delicious. If you have drip coffee on hand, feel free to use that as well, it’s what they use at Philz – I don’t have a coffee maker so I went with pour over. It sounds like a lot of mint, but every time (okay, the two times I went) I looked in my cup, there were at least 10-15 mint leaves muddled up. Trust me, you need the mint!",
+];
+
+export const dairyTitles = [
+  "How to Make Whipped Matcha (Dalgona Matcha) Two Ways + Tips",
+  "Bolognese",
+  "Homemade Clotted Cream",
+];
+
+export const dairyDescription = [
+  "Two different ways to make fluffy dalgona matcha for a perfect fluffy cafe matcha drink at home",
+  "How to make the best pasta ever with this silky smooth slow simmered bolognese sauce.",
+  "Homemade clotted cream – thick, rich, and perfect for spreading onto cream scones (or toast!) for afternoon tea.",
+];
+
+export const drinksTitles = [
+  "Negroni: the definitive cocktail",
+  "Whipped Lemonade",
+  "The Best Old Fashioned Cocktail Recipe",
+  "Manhattan",
+];
+
+export const drinksDescription = [
+  "How to make the best cocktail of last 100 years.",
+  "Whipped lemonade is a creamy sweet, lemony, and refreshing blended lemonade drink made with lemonade, ice, and cream.",
+  "How to make the greatest cocktail of all time.",
+  "The greatest cocktail from the greatest city",
+];
+
+export const fastFoodTitles = [
+  "A Breakdown of the Full English Breakfast",
+  "Soft Scrambled Eggs and Spicy Chorizo Breakfast Nachos",
+  "Tomato and Egg Breakfast Pizza Recipe",
+];
+
+export const fastFoodDescription = [
+  "Welcome to Weekend Brunch! Skip the lines and make brunch at home. The coffee’s truly bottomless and the best part is PJs all the way! This week: a guide to the gloriousness that is known as A Full English Breakfast.",
+  "Yup, you can eat nachos for breakfast, especially when they're topped with soft scrambled eggs, spicy chorizo, and ALL THE CHEESE you can handle.",
+  "While I was living in Japan, I ate my fill of Japanese foods: ramen, soba, udon, katsu, yakitori, curry, tempura, sushi, takoyaki, okonomiyaki, anything remotely Japanese, I was all over it. But sometimes, out of nowhere, a pizza craving would hit and the call for that piping hot crispy combination of savory tomato sauce and gooey cheese could not be ignored.",
+];
+
+export const greenVeggiesTitles = [
+  "Our 10 Best Instant Pot Recipes",
+  "Spicy Korean Ramen Recipe with Tofu and Vegetables",
+  "55 Best Easy Fall Recipes",
+];
+
+export const greenVeggiesDescription = [
+  "Here are our best Instant Pot recipes, the ones we turn to again and again.",
+  "Clean out your fridge with this spicy Korean stew recipe: 10 minutes and ALL THE SPICE.",
+  "These fall recipes will satisfy your cravings for cozy comfort food.",
+];
+
+export const highProteinTitles = [
+  "Chicken Caesar Salad",
+  "Shrimp Fried Rice",
+  "Chicken Satay",
+];
+
+export const highProteinDescription = [
+  "The best chicken caesar salad: crisp romaine lettuce, kale, garlicky croutons, air fried chicken breast, homemade caesar dressing, and so much more, because you're worth it.",
+  "Authentic shrimp fried rice with juicy plump shrimp, crispy rice, fluffy scrambled eggs, and salty, bombastic flavors.",
+  "Gorgeously grilled chicken satay skewers are perfect with a sweet-and-tangy no peanut peanut sauce.",
+];
+
+export const iceCreamTitles = [
+  "Brown Sugar Bacon Ice Cream Sandwich Recipe",
+  "Make bubble waffle ice cream cones at home: the best way to eat ice cream this summer",
+  "Carrot Cake Cream Cheese Pudding Pops Recipe",
+];
+
+export const iceCreamDescription = [
+  "Brown Sugar Bacon Bits Heat up a small non-stick skillet over low heat. Add the bacon and stir occasionally, rending out most of the fat. After about 15 minutes the tiny bacon bits should be super crispy and deep brown. If they aren’t, continue to brown until crispy. Remove the bits from the pan with a slotted spoon and drain on paper towels. Save the bacon fat for other uses if you wish, but either way, clean out your skillet.",
+  "Make your own infinitely instagram friendly bubble waffle cones/puffle cones/eggloo egg waffle cones right at home.",
+  "Creamy cheesecake pudding pops with chunks of fluffy carrot cake! Creamy, cold, sweet and cake-y, the best of all worlds :)",
+];
+
+export const lunchTitles = [
+  "Dry Mein Recipe from Aloha Kitchen",
+  "Stuffing Lovers Unite: 5 Ingredient 15 Minute Instant Pot Mushroom and Onion Stuffing",
+  "Sweet and Sour Vietnamese Hot Pot Recipe",
+];
+
+export const lunchDescription = [
+  "Skip the flight and make these noodles – it’s Hawaii on a plate.",
+  "In the Instant Pot insert, on high, melt the butter and sear the mushrooms. Sear until deeply golden, stirring only very occasionally. Stir in the onions and cook until soft, but not brown.",
+  "If you have a food processor, add the onions, garlic, ginger, shallots, and lemongrass. Blitz until fine. Cut the tomato in wedges and peel, core and roughly chop the pineapple.",
+];
+
+export const movieSnackTitles = [
+  "9 of the Best Low Carb Snacks and Snack Recipes",
+  "Strawberry Oatmeal Muffins",
+  "Taiwanese Popcorn Chicken",
+];
+
+export const movieSnackDescription = [
+  "Satisfying crunchy, savory, delicious low carb snacks and snack recipes",
+  "The perfect springtime muffin to remind you of warmer days ahead filled with summer strawberries.",
+  "Make this super crunchy, absolutely addictive snack with our ultimate guide to Taiwanese popcorn chicken.",
+];
+
+export const nonVegTitles = [
+  "The Juiciest Baked Chicken Thighs",
+  "Chicken Spaghetti",
+  "Tofu Fried Tofu Vegan Chicken Nugget Recipe",
+];
+
+export const nonVegDescription = [
+  "Perfect pantry juicy baked chicken thighs, every time!",
+  "Chicken spaghetti is pure comfort food goodness: tender pieces chicken mixed with spaghetti, tomatoes, and peppers in a creamy cheesy sauce.",
+  "These boot-shaped juicy chicken fried tofu nuggets inspired by NYC’s Superiority Burger will change your life.",
+];
+
+export const saladTitles = [
+  "The Ultimate Kale Salad",
+  "Sunomono Japanese Cucumber Salad",
+  "Chinese Cucumber Avocado Salad",
+];
+
+export const saladDescription = [
+  "This kale salad gets me in my fall feels.",
+  "How to make Japanese cucumber salad, aka sunomono. Cooling, crisp, and refreshing, it’s the perfect accompaniment to any meal.",
+  "Creamy buttery avocados offset this cool chinese cucumber salad.",
+];
+
+export const snacksTitles = [
+  "Pizza Toast Recipe",
+  "Chocolate and Vanilla Slider Cakes",
+  "Sunday Brunch: Matcha Portuguese Egg Tarts",
+];
+
+export const snacksDescription = [
+  "Pizza toast party: for a nostalgic taste right out of childhood, try these quick and easy pizza toasts!",
+  "It's Molly Yeh's book birthday and I'm celebrating with mini mini slider cakes! WOOT!",
+];
+
+export const sushiTitles = ["Sushi Rice", "Sushi Bake", "Oreo Sushi"];
+
+export const sushiDescriptions = [
+  "Sushi rice is the most important part of sushi, and you can instantly upgrade your homemade sushi with this simple recipe.",
+  "A savory, creamy, and super satisfying deconstructed California roll, layered and baked in a casserole.",
+  "A new way to eat oreos? I'm in!",
+];
+
+export const sweetsTitles = [
+  "The Best Combini Sweets to Try in Japan",
+  "The Search for Perfect Purin in Tokyo",
+  "All Edges Brownie Cookies",
+];
+
+export const sweetsDescriptions = [
+  "If you’re heading to Japan, don’t forget to hit up the combini!",
+  "Whether you call it pudding or purin, Tokyo has some of the best examples of pudding on the planet, and even if none of us can go there anytime soon, we can dream.",
+  "There’s something about baking cookies that makes a house super warm and inviting",
+];
+
+export const postDescriptionMap = {
+  breakfast: breakfastDescriptions,
+  cake: cakeDescription,
+  chocolate: chocolateDescription,
+  coffee: coffeeDescription,
+  dairy: dairyDescription,
+  drinks: drinksDescription,
+  "fast-food": fastFoodDescription,
+  "green-veggies": greenVeggiesDescription,
+  "high-protein-food": highProteinDescription,
+  "ice-cream": iceCreamDescription,
+  lunch: lunchDescription,
+  "movie-snack": movieSnackDescription,
+  "non-veg": nonVegDescription,
+  salad: saladDescription,
+  snack: snacksDescription,
+  sushi: sushiDescriptions,
+  sweets: sweetsDescriptions,
+};
+
+export const postTitleMap = {
+  breakfast: breakfastTitles,
+  cake: cakeTitles,
+  chocolate: chocolateTitles,
+  coffee: coffeeTitles,
+  dairy: dairyTitles,
+  drinks: drinksTitles,
+  "fast-food": fastFoodTitles,
+  "green-veggies": greenVeggiesTitles,
+  "high-protein-food": highProteinTitles,
+  "ice-cream": iceCreamTitles,
+  lunch: lunchTitles,
+  "movie-snack": movieSnackTitles,
+  "non-veg": nonVegTitles,
+  salad: saladTitles,
+  snack: snacksTitles,
+  sushi: sushiTitles,
+  sweets: sweetsTitles,
+};
---
+++ dummy-data/src/product/data.ts
@@ -0,0 +1,133 @@
+export const breakfastTitles = [
+  "Keto Biscuit Zero Carb Mix - Keto and Gluten Free Biscuit Baking Mix - 0g Net Carbs Per Biscuit - Easy to Bake - No Nut Flours - Makes 12 Biscuits (23g Mix)",
+  "2021 Kellogg's Cereal Straws Froot Loops Edible Breakfast Straw Alternatives for Milk, 90's Childhood Nostalgic Treat for Drinking and Eating, Cereals for Kids, Pack of 2, 36 Straws",
+  "Magic Spoon Cereal, Variety 8-Pack Single Serve Cups - Keto, Gluten & Grain Free, Low Carb, High Protein, Zero Sugar, Non-GMO Breakfast Cereal",
+  "FlapJacked Mighty Muffins, Power Breakfast Variety, 12 Pack | 20g Protein + Probiotics",
+  "Quaker Instant Oatmeal, Maple & Brown Sugar, Individual Packets, 48 Count",
+  "Nature's Path Organic Summer Berries Boost Instant Oatmeal Cup, 1.94 Ounce (Pack of 12), Non-GMO, Vitamin C Rich, 7g Plant Based Protein, with Omega-3 Rich Chia Seeds",
+  "POWERFUL, Instant Oatmeal Cup, 20g Protein, Kosher, Low Sugar (Peaches & Cream, Pack of 6)",
+];
+
+export const cakeTitles = [
+  "TORTUGA Caribbean Original Rum Cake with Walnuts - 32 oz Rum Cake - The Perfect Premium Gourmet Gift for Gift Baskets, Parties, Holidays, and Birthdays - Great Cakes for Delivery",
+  "Chiostro Di Saronno Panettone with Fruity Delicious Limoncello - Hand wrapped - Traditional Recipe Italian Christmas Cake in Giftable wrapping Imported from Italy 750g",
+  "Kodiak Cakes Minute Muffins, Mountain Blueberry, 2.29 Ounce (Pack of 12) (Packaging May Vary)",
+];
+
+export const chocolateTitles = [
+  "Atkins Endulge Treat Nutty Fudge Brownie Bar. Decadent Brownie Treat with Chocolatey Coating and Walnuts. Keto-Friendly. (5 Bars)",
+  "Ferrero Rocher Collection, Fine Hazelnut Milk Chocolates, 48 Count, Gift Box, Assorted Coconut Candy and Chocolates, Great for Holiday Entertaining, 18.2 oz",
+  "Whitman's Sampler Gift Box of Assorted Chocolates, 36 Ounce (72 Pieces)",
+  "DOVE PROMISES Dark Chocolate Candy 43.07 Ounce 150-Piece Bag",
+  "Lindt Swiss Luxury Selection Boxed Chocolate, Gift Box, Great for Holiday Gifting, 5.1 Ounce",
+  "Godiva 27Piece Belgium Boxed Assorted Chocolates, 11.1 Oz",
+  "Ghirardelli Assorted Squares Chocolates (Net Wt 23.8 Ounce )",
+];
+
+export const coffeeTitles = [
+  "McCafé Breakfast Blend Light Roast Ground Coffee (30 oz Canister)",
+  "Koffee Kult Coffee Beans Dark Roasted - Highest Quality Delicious Organically Sourced Fair Trade - Whole Bean Coffee - Fresh Gourmet Aromatic Artisan Blend (32oz)",
+  "Four Sigmatic Mushroom Coffee K-Cups, Organic and Fair Trade Coffee with Lions Mane, Chaga, & Mushroom Powder, Focus & Immune Support, 24 Count",
+  "4-PACK Nescafe 3-in-1 Original Blend and Brew Premix Instant Coffee (112 Sticks)",
+  "Nescafe Classic Instant Greek Coffee, 7.08 Ounce (Pack of 2)",
+  "Energy + Focus Coffee - Premium, Gourmet,100% South American Blend, Arabica, Robusta - w/Herbs for Memory, Clarity, Energy, & Focus - Bacopa, Gotu Kola, Made in USA, Javita 24 ct",
+];
+
+export const dairyTitles = [
+  "The Best of Europe Cheese Assortment (2 pound) - Finest Gourmet European Cheeses - A Variety Of French Cheese, English Cheese, Italian Cheese, and Spanish Cheese",
+  "Maple Hill Shelf Stable Whole White Milk 100% Grass Fed, Organic, Non-GMO: 12 Pack 8 Fl Oz Aseptic Milk Boxes",
+  "PARMALAT MILK - 2%, 32 OZ (pack of 6)",
+];
+
+export const drinksTitles = [
+  "Sparkling Ice Purple Variety Pack, Zero Sugar Sparkling Water, with Vitamins and Antioxidants, 17 fl oz, 12 count (Black Raspberry, Cherry Limeade, Orange Mango, Kiwi Strawberry)",
+  "BODYARMOR Sports Drink Sports Beverage, Orange Mango, Natural Flavors With Vitamins, Potassium-Packed Electrolytes, No Preservatives, Perfect For Athletes, 16 Fl Oz (Pack of 12)",
+  "CELSIUS Fitness Energy Drink, 12 Fl Oz, Standard Variety Pack (Pack of 12)",
+  "Monster Rehab Tea + Lemonade + Energy, Energy Iced Tea, 15.5 Ounce (Pack of 24)",
+];
+
+export const fastFoodTitles = [
+  "Frank's RedHot Original Cayenne Pepper Sauce (Keto Friendly), 12 fl oz",
+  "Chef Boyardee Pepperoni Pizza Maker, 31.85 Oz, 6 Pack",
+  "Weber Gluten Free Gourmet Burger Seasoning 19 Oz Super Size",
+];
+
+export const greenVeggiesTitles = [
+  "RightRice - Variety Pack (7oz. Pack of 6) - Made from Vegetables - High Protein, Vegan, non GMO, Gluten Free",
+  "Green Giant Vegetable Blend, 15 Ounce Can (Pack of 12)",
+  "Anthony's Textured Vegetable Protein, TVP, 1.5 lb, Gluten Free, Vegan, Made in USA, Unflavored",
+];
+
+export const highProteinTitles = [
+  "POWERFUL, Instant Oatmeal Cup, 20g Protein, Kosher, Low Sugar (Apple Cinnamon, 6 Pack)",
+  "Pure Protein Chocolate Peanut Butter Protein Bars, 1.76 oz, 12 Count",
+  "Quest Nutrition- High Protein, 2.12 Ounce Bars, 12 Count",
+];
+
+export const iceCreamTitles = [
+  "Junket Ice Cream Mix Bundle - 2 Vanilla, 2 Chocolate, 2 Strawberry (6 Total)",
+  "Triple Scoop Ice Cream Mix, Premium Chocolate, starter for use with home ice cream maker, no artificial colors or flavors, ready in under 30 mins, makes 2 qts (1 15oz box)",
+  "Dole Soft Serve Mix 4.75 Pound, pineapple, 70.4 Ounce",
+];
+
+export const lunchTitles = [
+  "Wonderful Pistachios & Almonds Roasted and Salted Pistachios,1.5 Ounce, Pack of 24.",
+  "Chef Boyardee Beef in Tomato & Meat Sauce Ravioli, 7.5 Oz. (Pack of 12)",
+  "StarKist Lunch To-Go Chunk Light Pouch - Mix Your Own Tuna Salad - (Packaging 4.1 Ounce (Pack of 12)",
+];
+
+export const movieSnacksTitles = [
+  "Quinn Movie Night Popcorn Variety Gift Kit, 3 Flavors - Real Butter, Parmesan & Rosemary, Non-Gmo, Organic Popcorn Kernels (9 bags)",
+  "Urban Accents MOVIE NIGHT Popcorn Kernels and Popcorn Seasoning Variety Pack (set of 8) - 3 Non-GMO Popcorn Kernel Packs and 5 Gourmet Popcorn Snack Seasoning- Perfect Gift for any Occasion",
+  "Ultimate Movie Night Gift Basket Snacks, Holiday Gift, Full Size candy variety POPCORN LOVER box for Kids, College Students Graduation gift, Family Gift or birthday Gift w/ Candy Tin Box",
+];
+
+export const nonVegTitles = [
+  "Nutristore Freeze-Dried Grilled Chicken | Premium Quality Pre-Cooked Chicken Breast | Survival Emergency Food Supply | Home Meals & Lightweight Camping | USDA Inspected | 25-Year Shelf Life",
+  "Kirkland Signature Chicken Breast, 12.5 oz (6)",
+  "Valley Fresh Organic White Chicken - 5 oz.-Pack of 12",
+];
+
+export const saladTitles = [
+  "naturSource Organic Salad Topper Smart Life Gluten Free",
+  "McCormick Perfect Pinch Salad Supreme Seasoning, 8.25 Oz (Packaging May Vary)",
+  "Orchard Valley Harvest Salad Toppers, Non-GMO, No Artificial Ingredients, Honey Roasted Sliced Almonds & Berries, 0.85 Oz, Pack of 8",
+];
+
+export const snackTitles = [
+  "Lay's Potato Chip Variety Pack, 1 Ounce (Pack of 40)",
+  "Welch's Fruit Snacks, Mixed Fruit, Gluten Free, Bulk Pack, 0.9 oz Individual Single Serve Bags 40 Count (Pack of 1)",
+  "Welch's Fruit Snacks, Berries 'n Cherries, Gluten Free, Bulk Pack, 0.9 oz Individual Single Serve Bags (Pack of 40)",
+];
+
+export const sushiTitles = [
+  "Lundberg Family Farms - Organic California Sushi Rice, Japanese-style Short Grain, Perfectly Sticky, Bulk Rice, Pantry Staple, Gluten-Free, Non-GMO, USDA Certified Organic, Vegan, Kosher (64 oz)",
+  "Soeos Premium Sushi Rice, Calrose White Rice, Dried White Rice, White Sicky Rice, Best Rice for Sushi, 5 Pound",
+  "Nishiki Premium Sushi Rice, 10lbs",
+];
+
+export const sweetsTitles = [
+  "SmartSweets Variety Pack for Adults, Candy with Low Sugar & Calorie, New Years Gift Idea, Snack Care Package - Sweet Fish, Sourmelon Bites, Peach Rings, Sour Blast Buddies, 1.8oz (Pack of 8)",
+  "Sweet & Low sugar substitute (Case of 1500)",
+  "Smart Sweets Cola Gummies, Candy with Low Sugar (3g), Low Calorie (100), No Artificial Sweeteners, Vegan, Plant-Based, Gluten-Free, Non-GMO, Healthy Snack for Kids & Adults, 1.8oz (Pack of 12)",
+];
+
+export const productTitles = {
+  breakfast: breakfastTitles,
+  cake: cakeTitles,
+  chocolate: chocolateTitles,
+  coffee: coffeeTitles,
+  dairy: dairyTitles,
+  drinks: drinksTitles,
+  "fast-food": fastFoodTitles,
+  "green-veggies": greenVeggiesTitles,
+  "high-protein-food": highProteinTitles,
+  "ice-cream": iceCreamTitles,
+  lunch: lunchTitles,
+  "movie-snack": movieSnacksTitles,
+  "non-veg": nonVegTitles,
+  salad: saladTitles,
+  snack: snackTitles,
+  sushi: sushiTitles,
+  sweets: sweetsTitles,
+};
