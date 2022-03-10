25f853bb7ea6d99c3bc040498717039fd25282a4
AkashSDas <aakashdas368@gmail.com>
2022-03-10 08:25:04 +0530

    create get all product orders controller

---
+++ backend/src/controllers/exam.ts
@@ -0,0 +1,13 @@
+import ProductOrder, { ProductOrderDocument } from "../models/product_order";
+import { Controller, responseMsg, runAsync } from "../utils";
+
+export const getProductOrders: Controller = async (req, res) => {
+  const [orders, err] = await runAsync(ProductOrder.find().exec());
+  if (err) return responseMsg(res);
+  return responseMsg(res, {
+    status: 200,
+    error: false,
+    msg: "Orders retrieved",
+    data: { orders },
+  });
+};
